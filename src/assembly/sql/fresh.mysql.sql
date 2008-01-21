6a4425bae56d6499383ae469e9bc25cd145b9d58
Thies Edeling <thies@te-con.nl>
2008-01-21 00:27:44 +0000

    release preparation

--- src/assembly/sql/fresh.mysql.sql
+++ src/assembly/sql/fresh.mysql.sql
@@ -26,7 +26,7 @@ CREATE TABLE `CONFIGURATION` (
 
 LOCK TABLES `CONFIGURATION` WRITE;
 /*!40000 ALTER TABLE `CONFIGURATION` DISABLE KEYS */;
-INSERT INTO `CONFIGURATION` VALUES ('completeDayHours','8'),('showTurnOver','true'),('localeLanguage','en'),('currency','Euro'),('localeCountry',NULL),('availableTranslations','en,nl'),('mailFrom','noreply@localhost.net'),('mailSmtp','127.0.0.1'),('demoMode','false'),('version', '0.7');
+INSERT INTO `CONFIGURATION` VALUES ('completeDayHours','8'),('showTurnOver','true'),('localeLanguage','en'),('currency','Euro'),('localeCountry',NULL),('availableTranslations','en,nl'),('mailFrom','noreply@localhost.net'),('mailSmtp','127.0.0.1'),('demoMode','false'),('version', '0.7.2');
 /*!40000 ALTER TABLE `CONFIGURATION` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,6 +195,8 @@ CREATE TABLE `TIMESHEET_ENTRY` (
   `ASSIGNMENT_ID` int(11) NOT NULL,
   `ENTRY_DATE` date NOT NULL,
   `HOURS` float(9,3) NOT NULL,
+  `COMMENT` varchar(2048),
+  `DISPLAY_ORDER` INT NOT NULL DEFAULT 1,  
   PRIMARY KEY  (`ENTRY_DATE`,`ASSIGNMENT_ID`),
   KEY `ASSIGNMENT_ID` (`ASSIGNMENT_ID`),
   CONSTRAINT `TIMESHEET_ENTRY_fk` FOREIGN KEY (`ASSIGNMENT_ID`) REFERENCES `PROJECT_ASSIGNMENT` (`ASSIGNMENT_ID`)
