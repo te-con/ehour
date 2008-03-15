3b473a74cfaf4e126670a77f4e22e4293642db75
Thies Edeling <thies@te-con.nl>
2008-03-15 17:15:43 +0000

    UPDATE_DATE added to ENTRY. Assignment status check improvement (archi)

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/session/EhourWebSession.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/session/EhourWebSession.java
@@ -156,8 +156,6 @@ public class EhourWebSession extends AuthenticatedWebSession
 		
 		// Create an Acegi authentication request.
 		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(u, p);
-
-		logger.info("Going to authenticate '" + username + "' with '" + password + "'");
 		
 		// Attempt authentication.
 		try
