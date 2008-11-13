c37a01bb5464014dbf8405bc38df7b90bd04e3dd
Thies Edeling <thies@te-con.nl>
2008-11-13 09:58:37 +0000

    ASSIGNED - # 57: Transaction/audit log http://www.ehour.nl/defects/view.php?id=57

--- eHour-wicketweb/src/test/java/net/rrm/ehour/ui/audit/page/AuditReportPageTest.java
+++ eHour-wicketweb/src/test/java/net/rrm/ehour/ui/audit/page/AuditReportPageTest.java
@@ -3,8 +3,15 @@
  */
 package net.rrm.ehour.ui.audit.page;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+
+import java.util.ArrayList;
+
+import net.rrm.ehour.audit.service.dto.AuditReportRequest;
+import net.rrm.ehour.domain.Audit;
 import net.rrm.ehour.ui.common.BaseUIWicketTester;
 
 import org.junit.Test;
@@ -20,6 +27,12 @@ public class AuditReportPageTest extends BaseUIWicketTester
 	@Test
 	public void testRender()
 	{
+		expect(auditService.getAuditCount(isA(AuditReportRequest.class)))
+			.andReturn(5);
+
+		expect(auditService.getAudit(isA(AuditReportRequest.class)))
+			.andReturn(new ArrayList<Audit>());
+		
 		replay(auditService);
 		
 		tester.startPage(AuditReportPage.class);
