6ba8cf11d5e5c8989a4ba32794f077a297ad1b75
Thies Edeling <thies@te-con.nl>
2009-10-21 20:23:03 +0000

    OPEN - issue EHO-160: Easier assignment of users to projects http://www.ehour.nl/jira/browse/EHO-160
    * id wrongly placed

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/admin/project/panel/addusers/AddUserPanel.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/admin/project/panel/addusers/AddUserPanel.java
@@ -3,12 +3,16 @@ package net.rrm.ehour.ui.admin.project.panel.addusers;
 import java.util.List;
 
 import net.rrm.ehour.domain.Project;
+import net.rrm.ehour.domain.ProjectAssignment;
 import net.rrm.ehour.domain.User;
 import net.rrm.ehour.domain.UserRole;
+import net.rrm.ehour.ui.admin.assignment.dto.AssignmentAdminBackingBeanImpl;
 import net.rrm.ehour.ui.admin.assignment.panel.AssignmentFormPanel;
+import net.rrm.ehour.ui.admin.assignment.panel.AssignmentFormPanel.DisplayOption;
 import net.rrm.ehour.ui.common.panel.AbstractBasePanel;
 import net.rrm.ehour.user.service.UserService;
 
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class AddUserPanel extends AbstractBasePanel
@@ -25,6 +29,10 @@ public class AddUserPanel extends AbstractBasePanel
 		List<User> users = userService.getUsers(UserRole.CONSULTANT);
 		
 		add(new ListUsersPanel("users", users));
-//		add(new AssignmentFormPanel(id, model))
+		
+		ProjectAssignment assignment = new ProjectAssignment();
+		AssignmentAdminBackingBeanImpl bBean = new AssignmentAdminBackingBeanImpl(assignment);
+		
+		add(new AssignmentFormPanel("assignmentInfo", new CompoundPropertyModel(bBean), DisplayOption.HIDE_DELETE_BUTTON, DisplayOption.HIDE_PROJECT_SELECTION));
 	}
 }
