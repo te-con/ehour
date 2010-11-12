431b9af7a8e2e6ba5c12a5db9d1e7f9e4318c455
Thies Edeling <thies@te-con.nl>
2010-11-12 12:19:49 +0000

    ehour.home can now be set to an absolute location, no need to have it on the classpath

--- eHour-standalone/src/main/java/net/rrm/ehour/ServerPropertiesConfigurator.java
+++ eHour-standalone/src/main/java/net/rrm/ehour/ServerPropertiesConfigurator.java
@@ -1,76 +1,82 @@
 package net.rrm.ehour;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.io.*;
+import java.util.Properties;
+
 public class ServerPropertiesConfigurator
 {
-	private final static Logger LOGGER = Logger.getLogger(ServerPropertiesConfigurator.class);
-	
-	public ServerConfig configureFromProperties(String filename) throws IOException
-	{
-		Properties props = loadProperties(filename);
-		
-		ServerConfig builder = new ServerConfig()
-						.setDataBase(props.getProperty("ehour.database"))
-						.setDataBaseDriver(props.getProperty("ehour.database.driver"))
-						.setDataBaseURL(props.getProperty("ehour.database.url"))
-						.setDataBaseUsername(props.getProperty("ehour.database.username"))
-						.setDataBasePassword(props.getProperty("ehour.database.password"))
-						.setPort(parseServerPort(props))
-						.setDefaultConfigFileName(props.getProperty("jetty.config.location"))
-						;
-		
-		return builder;
-		
-	}
+    private final static Logger LOGGER = Logger.getLogger(ServerPropertiesConfigurator.class);
 
-	private Integer parseServerPort(Properties props)
-	{
-		String serverPort = props.getProperty("ehour.standalone.port");
-		Integer port = null;
-		
-		if (StringUtils.isNotBlank(serverPort)) {
-			
-			try
-			{
-				port = Integer.valueOf(serverPort);
-			}
-			catch (NumberFormatException nfe) {
-				LOGGER.warn("Invalid port " + serverPort + ", using default port");
-			}
-		}
-		return port;
-	}
+    public ServerConfig configureFromProperties(String filename) throws IOException
+    {
+        Properties props = loadProperties(filename);
 
-	private Properties loadProperties(String filename) throws FileNotFoundException, IOException
-	{
-		Properties props = new Properties();
-		
-		File file = new File(filename);
-		
-		if (file.exists())
-		{
-			props.load(new FileInputStream(file));
-		} else
-		{
-			InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
+        ServerConfig builder = new ServerConfig()
+                .setDataBase(props.getProperty("ehour.database"))
+                .setDataBaseDriver(props.getProperty("ehour.database.driver"))
+                .setDataBaseURL(props.getProperty("ehour.database.url"))
+                .setDataBaseUsername(props.getProperty("ehour.database.username"))
+                .setDataBasePassword(props.getProperty("ehour.database.password"))
+                .setPort(parseServerPort(props))
+                .setDefaultConfigFileName(props.getProperty("jetty.config.location"));
 
-			if (inputStream == null)
-			{
-				throw new FileNotFoundException("property file '" + filename + "' not found in the classpath");
-			}
+        return builder;
 
-			props.load(inputStream);
+    }
 
-		}
-		return props;
-	}
+    private Integer parseServerPort(Properties props)
+    {
+        String serverPort = props.getProperty("ehour.standalone.port");
+        Integer port = null;
+
+        if (StringUtils.isNotBlank(serverPort))
+        {
+
+            try
+            {
+                port = Integer.valueOf(serverPort);
+            }
+            catch (NumberFormatException nfe)
+            {
+                LOGGER.warn("Invalid port " + serverPort + ", using default port");
+            }
+        }
+        return port;
+    }
+
+    private Properties loadProperties(String filename) throws FileNotFoundException, IOException
+    {
+        Properties props = new Properties();
+
+        File file = new File(filename);
+
+        if (file.exists())
+        {
+            props.load(new FileInputStream(file));
+        } else
+        {
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
+
+            if (inputStream == null)
+            {
+                LOGGER.debug("property file '" + filename + "' not found in the classpath, using it as absolute path");
+
+
+                try
+                {
+                    inputStream = new FileInputStream(filename);
+                } catch (FileNotFoundException e)
+                {
+                    throw new FileNotFoundException("property field " + filename + " not found in classpath or as absolute path");
+                }
+            }
+
+            props.load(inputStream);
+
+        }
+        return props;
+    }
 }
