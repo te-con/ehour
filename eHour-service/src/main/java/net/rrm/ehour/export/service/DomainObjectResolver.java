a04e68d2d2afef597a0510a200c170e8041b7816
Thies Edeling <thies@te-con.nl>
2010-11-17 22:45:43 +0000

    EHO-203 - Add import  from external file

--- eHour-service/src/main/java/net/rrm/ehour/export/service/DomainObjectResolver.java
+++ eHour-service/src/main/java/net/rrm/ehour/export/service/DomainObjectResolver.java
@@ -3,7 +3,13 @@ package net.rrm.ehour.export.service;
 import net.rrm.ehour.persistence.export.dao.ExportType;
 
 import javax.persistence.Column;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author thies (Thies Edeling - thies@te-con.nl)
@@ -11,9 +17,53 @@ import java.lang.reflect.Field;
  */
 public class DomainObjectResolver
 {
-    public <T> T parse(ExportType type, Class<T> clazz) throws IllegalAccessException, InstantiationException
+    public <T> T parse(ExportType type, Class<T> clazz, XMLEvent event, XMLEventReader reader) throws IllegalAccessException, InstantiationException, XMLStreamException
     {
         T domainObject = clazz.newInstance();
+
+        Map<String, Field> fieldMap = createFieldMap(clazz);
+
+        recursiveFillObject(domainObject, fieldMap, event, reader);
+
+        return domainObject;
+    }
+
+    private <T> void recursiveFillObject(T domainObject, Map<String, Field> fieldMap, XMLEvent event, XMLEventReader reader) throws XMLStreamException
+    {
+        if (event.isStartElement())
+        {
+            XMLEvent domainEvent = reader.nextEvent();
+
+            StartElement startElement = event.asStartElement();
+            String dbField = startElement.getName().getLocalPart();
+            Field field = fieldMap.get(dbField);
+
+            System.out.println(startElement);
+
+            XMLEvent characterEvent = reader.nextEvent();
+
+//            System.out.println(characterEvent);
+
+            if (characterEvent.isCharacters())
+            {
+                String data = characterEvent.asCharacters().getData();
+
+                Class<?> type = field.getType();
+
+                System.out.println(type);
+                if (type == Integer.class) {
+
+                }
+
+            }
+
+
+        }
+    }
+
+    private <T> Map<String, Field> createFieldMap(Class<T> clazz)
+    {
+        Map<String, Field> fieldMap = new HashMap<String, Field>();
 
         Field[] fields = clazz.getDeclaredFields();
 
@@ -24,10 +74,10 @@ public class DomainObjectResolver
                 Column column = field.getAnnotation(Column.class);
                 String columnName = column.name();
 
-                System.out.println(columnName + " on " + field.getName());
+                fieldMap.put(columnName, field);
             }
         }
 
-        return domainObject;
+        return fieldMap;
     }
 }
--- eHour-service/src/test/groovy/net/rrm/ehour/export/service/DomainObjectResolverTest.groovy
+++ eHour-service/src/test/groovy/net/rrm/ehour/export/service/DomainObjectResolverTest.groovy
@@ -1,5 +1,7 @@
 package net.rrm.ehour.export.service
 
+import javax.xml.stream.XMLEventReader
+import javax.xml.stream.XMLInputFactory
 import net.rrm.ehour.persistence.export.dao.ExportType
 import org.junit.Test
 
@@ -12,10 +14,23 @@ class DomainObjectResolverTest {
   @Test
   void displayAnnotations()
   {
+    XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+    XMLEventReader eventReader = inputFactory.createXMLEventReader(new StringReader("""<TIMESHEET_ENTRY>
+   <ASSIGNMENT_ID>5</ASSIGNMENT_ID>
+   <ENTRY_DATE>2008-10-29</ENTRY_DATE>
+   <HOURS>2.0</HOURS>
+   <UPDATE_DATE>2008-11-03 23:20:42.0</UPDATE_DATE>
+  </TIMESHEET_ENTRY>"""));
+
     def resolver = new DomainObjectResolver();
 
     def type = ExportType.TIMESHEET_ENTRY;
 
-    resolver.parse(type, type.getDomainObjectClass());
+    // skip the startdoc
+    eventReader.nextEvent();
+
+    def event = eventReader.nextEvent();
+
+    resolver.parse(type, type.getDomainObjectClass(), event, eventReader);
   }
 }
