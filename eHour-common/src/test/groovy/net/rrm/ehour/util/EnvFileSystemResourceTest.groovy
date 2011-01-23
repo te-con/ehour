d210abb54e5ee9ed3997bb67da12a7bbacd64295
Thies Edeling <thies@te-con.nl>
2011-01-23 21:11:21 +0000

    windows file separator fix

--- eHour-common/src/test/groovy/net/rrm/ehour/util/EnvFileSystemResourceTest.groovy
+++ eHour-common/src/test/groovy/net/rrm/ehour/util/EnvFileSystemResourceTest.groovy
@@ -8,23 +8,25 @@ import org.junit.Test
  */
 class EnvFileSystemResourceTest
 {
+	private static final String SEPARATOR = System.getProperty("file.separator")
+
 
   @Test
   void shouldParseEnv()
   {
     System.properties.put "EHOUR_HOME", "ok";
 
-    def resource = new EnvFileSystemResource("\${EHOUR_HOME}/test")
+    def resource = new EnvFileSystemResource("\${EHOUR_HOME}${SEPARATOR}test")
 
-    assert resource.file.path == "ok/test"
+    assert resource.file.path == "ok${SEPARATOR}test"
   }
 
   @Test
   void shouldParseWithoutEnv()
   {
-    def resource = new EnvFileSystemResource("ok/test")
+    def resource = new EnvFileSystemResource("ok${SEPARATOR}test")
 
-    assert resource.file.path == "ok/test"
+    assert resource.file.path == "ok${SEPARATOR}test"
   }
 
 }
