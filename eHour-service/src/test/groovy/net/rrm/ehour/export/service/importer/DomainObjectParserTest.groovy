6dfb1f1abf71964df85c6a976b750e955f70e0bd
Thies Edeling <thies@te-con.nl>
2012-02-02 20:52:09 +0000

    EHO-252: Didn't compile with java 7: incorrect signature

--- eHour-service/src/test/groovy/net/rrm/ehour/export/service/importer/DomainObjectParserTest.groovy
+++ eHour-service/src/test/groovy/net/rrm/ehour/export/service/importer/DomainObjectParserTest.groovy
@@ -15,46 +15,41 @@ import static org.junit.Assert.*
  * @author thies (Thies Edeling - thies@te-con.nl)
  * Created on: Nov 16, 2010 - 11:54:54 PM
  */
-class DomainObjectParserTest
-{
-  DomainObjectParserDaoTestValidator daoValidator;
-  PrimaryKeyCache keyCache
-  ParseSession status
+class DomainObjectParserTest {
+    DomainObjectParserDaoTestValidator daoValidator;
+    PrimaryKeyCache keyCache
+    ParseSession status
 
-  @Mock
-  DomainObjectParserDao parserDao
+    @Mock
+    DomainObjectParserDao parserDao
 
-  @Before
-  void setUp()
-  {
-    keyCache = new PrimaryKeyCache()
-    status = new ParseSession()
+    @Before
+    void setUp() {
+        keyCache = new PrimaryKeyCache()
+        status = new ParseSession()
 
-    MockitoAnnotations.initMocks this
-  }
+        MockitoAnnotations.initMocks this
+    }
 
-  private DomainObjectParser createResolver(String xmlData)
-  {
-    return createResolver(xmlData, null, null)
-  }
+    private DomainObjectParser createResolver(String xmlData) {
+        return createResolver(xmlData, null, null)
+    }
 
-  private DomainObjectParser createResolver(String xmlData, def returnOnFind, def onFind)
-  {
-    XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-    XMLEventReader eventReader = inputFactory.createXMLEventReader(new StringReader(xmlData))
+    private DomainObjectParser createResolver(String xmlData, def returnOnFind, def onFind) {
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        XMLEventReader eventReader = inputFactory.createXMLEventReader(new StringReader(xmlData))
 
-    // skip the startdoc
-    eventReader.nextTag()
+        // skip the startdoc
+        eventReader.nextTag()
 
-    daoValidator = (returnOnFind == null) ? new DomainObjectParserDaoValidatorImpl() : new DomainObjectParserDaoTestValidator(returnOnFind, onFind)
+        daoValidator = (returnOnFind == null) ? new DomainObjectParserDaoValidatorImpl() : new DomainObjectParserDaoTestValidator(returnOnFind, onFind)
 
-    return new DomainObjectParser(eventReader, daoValidator, keyCache);
-  }
+        return new DomainObjectParser(eventReader, daoValidator, keyCache);
+    }
 
-  @Test
-  void shouldParseTwoTimesheetEntries()
-  {
-    def resolver = createResolver("""<TIMESHEET_ENTRIES CLASS="net.rrm.ehour.domain.TimesheetEntry">
+    @Test
+    void shouldParseTwoTimesheetEntries() {
+        def resolver = createResolver("""<TIMESHEET_ENTRIES CLASS="net.rrm.ehour.domain.TimesheetEntry">
   <TIMESHEET_ENTRY>
    <ASSIGNMENT_ID>1</ASSIGNMENT_ID>
    <ENTRY_DATE>2007-03-26</ENTRY_DATE>
@@ -69,28 +64,27 @@ class DomainObjectParserTest
   </TIMESHEET_ENTRIES>
 """, ProjectAssignmentMother.createProjectAssignment(1), 1)
 
-    def type = ExportType.TIMESHEET_ENTRY;
+        def type = ExportType.TIMESHEET_ENTRY;
 
-    keyCache.putKey(ProjectAssignment.class, 1, 1)
-    keyCache.putKey(ProjectAssignment.class, 2, 2)
+        keyCache.putKey(ProjectAssignment.class, 1, 1)
+        keyCache.putKey(ProjectAssignment.class, 2, 2)
 
-    List<TimesheetEntry> result = resolver.parse(type.getDomainObjectClass(), status);
+        List<TimesheetEntry> result = resolver.parse(type.getDomainObjectClass(), status);
 
-    assertEquals 2, result.size()
+        assertEquals 2, result.size()
 
-    assertNotNull result[0].entryId.entryDate
-    assertNotNull result[0].entryId.projectAssignment
-    assertEquals 8.0, result[0].hours, 0
-    assertEquals "jaja", result[0].comment
-    assertEquals 2, daoValidator.totalPersistCount
-  }
+        assertNotNull result[0].entryId.entryDate
+        assertNotNull result[0].entryId.projectAssignment
+        assertEquals 8.0, result[0].hours, 0
+        assertEquals "jaja", result[0].comment
+        assertEquals 2, daoValidator.totalPersistCount
+    }
 
-  @Test
-  void shouldParseUserAndStoreNewKeyInCacheMap()
-  {
-    def department = UserDepartmentMother.createUserDepartment()
+    @Test
+    void shouldParseUserAndStoreNewKeyInCacheMap() {
+        def department = UserDepartmentMother.createUserDepartment()
 
-    def resolver = createResolver("""<USERLIST>
+        def resolver = createResolver("""<USERLIST>
   <USERS>
    <USER_ID>1</USER_ID>
    <USERNAME>admin</USERNAME>
@@ -115,32 +109,31 @@ class DomainObjectParserTest
   </USERLIST>
 """, department, 1)
 
-    def type = ExportType.USERS
+        def type = ExportType.USERS
 
-    keyCache.putKey(UserDepartment.class, 1, 1)
+        keyCache.putKey(UserDepartment.class, 1, 1)
 
-    List<User> result = resolver.parse(type.getDomainObjectClass(), status);
+        List<User> result = resolver.parse(type.getDomainObjectClass(), status);
 
-    assertEquals 2, result.size()
+        assertEquals 2, result.size()
 
-    assertEquals "admin", result[0].username
-    assertEquals "1d798ca9dba7df61bf399a02695f9f50034bad66", result[0].password
-    assertEquals "eHour", result[0].firstName
-    assertEquals "Admin", result[0].lastName
-    assertEquals department, result[0].userDepartment
-    assertEquals "t@t.net", result[0].email
-    assertTrue result[0].active
-    assertFalse resolver.keyCache.isEmpty()
+        assertEquals "admin", result[0].username
+        assertEquals "1d798ca9dba7df61bf399a02695f9f50034bad66", result[0].password
+        assertEquals "eHour", result[0].firstName
+        assertEquals "Admin", result[0].lastName
+        assertEquals department, result[0].userDepartment
+        assertEquals "t@t.net", result[0].email
+        assertTrue result[0].active
+        assertFalse resolver.keyCache.isEmpty()
 
-    def userPk = resolver.keyCache.getKey(User.class, 3)
-    assertNotNull userPk
-    assertEquals "2", userPk
-  }
+        def userPk = resolver.keyCache.getKey(User.class, 3)
+        assertNotNull userPk
+        assertEquals "2", userPk
+    }
 
-  @Test
-  void shouldParseEnum()
-  {
-    def resolver = createResolver(""" <AUDITS CLASS="net.rrm.ehour.domain.Audit"><AUDIT>
+    @Test
+    void shouldParseEnum() {
+        def resolver = createResolver(""" <AUDITS CLASS="net.rrm.ehour.domain.Audit"><AUDIT>
    <AUDIT_ID>173</AUDIT_ID>
    <USER_ID>2</USER_ID>
    <USER_FULLNAME>Edeling, Thies</USER_FULLNAME>
@@ -150,21 +143,20 @@ class DomainObjectParserTest
   </AUDIT></AUDITS>
 """, UserMother.createUser(), 2)
 
-    def type = ExportType.AUDIT
+        def type = ExportType.AUDIT
 
-    List<Audit> result = resolver.parse(type.getDomainObjectClass(), status);
+        List<Audit> result = resolver.parse(type.getDomainObjectClass(), status);
 
-    assertEquals 1, result.size()
+        assertEquals 1, result.size()
 
-    assertEquals AuditActionType.LOGIN, result[0].auditActionType
-  }
+        assertEquals AuditActionType.LOGIN, result[0].auditActionType
+    }
 
-  @Test
-  void shouldRetrieveManyToOne()
-  {
-    def user = UserMother.createUser()
+    @Test
+    void shouldRetrieveManyToOne() {
+        def user = UserMother.createUser()
 
-    def resolver = createResolver(""" <AUDITS CLASS="net.rrm.ehour.domain.Audit"><AUDIT>
+        def resolver = createResolver(""" <AUDITS CLASS="net.rrm.ehour.domain.Audit"><AUDIT>
    <AUDIT_ID>173</AUDIT_ID>
    <USER_ID>2</USER_ID>
    <USER_FULLNAME>Edeling, Thies</USER_FULLNAME>
@@ -174,33 +166,31 @@ class DomainObjectParserTest
   </AUDIT></AUDITS>
 """, user, 2)
 
-    def type = ExportType.AUDIT
+        def type = ExportType.AUDIT
 
-    keyCache.putKey(User.class, 2, 2)
+        keyCache.putKey(User.class, 2, 2)
 
-    List<Audit> result = resolver.parse(type.getDomainObjectClass(), status);
+        List<Audit> result = resolver.parse(type.getDomainObjectClass(), status);
 
-    assertEquals 1, result.size()
+        assertEquals 1, result.size()
 
-    assertEquals AuditActionType.LOGIN, result[0].auditActionType
-    assertEquals 1, daoValidator.totalPersistCount
-    assertEquals user, result[0].user
-  }
-
-  private class DomainObjectParserDaoTestValidator<T> extends DomainObjectParserDaoValidatorImpl
-  {
-    private T returnObject;
-    private Serializable primaryKey;
-
-    DomainObjectParserDaoTestValidator(T returnObject, Serializable primaryKey)
-    {
-      this.primaryKey = primaryKey;
-      this.returnObject = returnObject;
+        assertEquals AuditActionType.LOGIN, result[0].auditActionType
+        assertEquals 1, daoValidator.totalPersistCount
+        assertEquals user, result[0].user
     }
 
-    public <T> T find(Serializable pk, Class<T> type)
-    {
-      return pk.equals(this.primaryKey) ? returnObject : null
-    }
-  };
+    private class DomainObjectParserDaoTestValidator<T> extends DomainObjectParserDaoValidatorImpl {
+        private T returnObject;
+        private Serializable primaryKey;
+
+        DomainObjectParserDaoTestValidator(T returnObject, Serializable primaryKey) {
+            this.primaryKey = primaryKey;
+            this.returnObject = returnObject;
+        }
+
+        @Override
+        public <T extends Serializable> T find(Serializable pk, Class<T> type) {
+            return pk.equals(this.primaryKey) ? returnObject : null as T
+        }
+    };
 }
