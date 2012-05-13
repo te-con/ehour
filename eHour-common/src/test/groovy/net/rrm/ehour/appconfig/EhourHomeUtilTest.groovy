5fc882bd7bc506789d155ba029afa9edf56107a4
thies <thies@te-con.nl>
2012-05-14 01:00:23 +0200

    EHO-226 - failed on Windows, depended on system's file separator.

--- eHour-common/src/test/groovy/net/rrm/ehour/appconfig/EhourHomeUtilTest.groovy
+++ eHour-common/src/test/groovy/net/rrm/ehour/appconfig/EhourHomeUtilTest.groovy
@@ -6,16 +6,11 @@ import org.junit.Test
  * @author thies (Thies Edeling - thies@te-con.nl)
  * Created on: 1/8/11 - 3:19 PM
  */
-class EhourHomeUtilTest
-{
-  @Test
-  void shouldConstructConfDir()
-  {
-    def dir = EhourHomeUtil.getConfDir("a")
+class EhourHomeUtilTest {
+    @Test
+    void shouldConstructConfDir() {
+        def dir = EhourHomeUtil.getConfDir("a")
 
-    def sep = System.getProperty("file.separator");
-
-    assert dir == "a${sep}conf${sep}"
-
-  }
+        assert dir == "a/conf/"
+    }
 }
