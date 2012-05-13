1e85ad2e56e278c8f1f3a6f78c1bd93e6f000a56
thies <thies@te-con.nl>
2012-05-14 01:00:50 +0200

    EHO-226 - removed duplication, reused convenience methods on EhourHomeUtil

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/listener/EnvInitListener.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/listener/EnvInitListener.java
@@ -12,54 +12,39 @@ import javax.servlet.ServletContextListener;
  * @author thies (Thies Edeling - thies@te-con.nl)
  *         Created on: 1/23/11 - 2:17 AM
  */
-public class EnvInitListener implements ServletContextListener
-{
+public class EnvInitListener implements ServletContextListener {
     private static final Logger LOG = Logger.getLogger(EnvInitListener.class);
 
     @Override
-    public void contextInitialized(ServletContextEvent sce)
-    {
+    public void contextInitialized(ServletContextEvent sce) {
         String home = getEhourHomePath(sce);
 
-        if (StringUtils.isBlank(home))
-        {
+        if (StringUtils.isBlank(home)) {
             throw new IllegalArgumentException("EHOUR_HOME environment variable or context parameter not defined - exiting");
         }
 
-        System.getProperties().put(EhourHomeUtil.EHOUR_HOME, home);
+        EhourHomeUtil.setEhourHome(home);
 
-        configureLog4j(home);
+        configureLog4j();
 
         LOG.info("EHOUR_HOME set to " + home);
     }
 
-    private String getEhourHomePath(ServletContextEvent sce)
-    {
+    private String getEhourHomePath(ServletContextEvent sce) {
         String home = EhourHomeUtil.getEhourHome();
 
-        if (StringUtils.isBlank(home))
-        {
+        if (StringUtils.isBlank(home)) {
             home = sce.getServletContext().getInitParameter(EhourHomeUtil.EHOUR_HOME);
         }
 
         return home;
     }
 
-    private void configureLog4j(String eHourHome)
-    {
-        String separator = System.getProperty("file.separator");
-
-        StringBuilder log4jConfigPath = new StringBuilder(eHourHome);
-        log4jConfigPath.append(separator);
-        log4jConfigPath.append("conf");
-        log4jConfigPath.append(separator);
-        log4jConfigPath.append("log4j.properties");
-
-        PropertyConfigurator.configure(log4jConfigPath.toString());
+    private void configureLog4j() {
+        PropertyConfigurator.configure(EhourHomeUtil.getFileInConfDir("log4j.properties").getAbsolutePath());
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent sce)
-    {
+    public void contextDestroyed(ServletContextEvent sce) {
     }
 }
