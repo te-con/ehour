3c2488b29030f22a5536aea143b16706e5f0cdb1
Thies Edeling <thies@te-con.nl>
2012-05-20 12:46:48 +0200

    EHO-251 leftover PM roles but without project (bug in eHour < 0.9.1) are now cleaned upon startup

---
+++ eHour-service/src/main/java/net/rrm/ehour/init/EmptyPmRoleDeletor.java
@@ -0,0 +1,28 @@
+package net.rrm.ehour.init;
+
+import net.rrm.ehour.persistence.user.dao.UserDao;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * In version < 0.9.1 there was an issue with the PM role, it was not deleted properly.
+ * This class is a cleanup job, when eHour is started it deletes the PM userrole when a
+ * user is not listed as PM in a project.
+ */
+
+@Service
+public class EmptyPmRoleDeletor {
+    private final static Logger LOGGER = Logger.getLogger(EmptyPmRoleDeletor.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    @PostConstruct
+    public void cleanUpPmRoles() {
+        LOGGER.info("Cleaning up leftover Projectmanagement roles without project...");
+        userDao.deletePmWithoutProject();
+    }
+}
