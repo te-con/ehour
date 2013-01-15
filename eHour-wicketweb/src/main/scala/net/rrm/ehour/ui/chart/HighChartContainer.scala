e4157605cdb7a0f9d5a0616a22bfa7b12d364e22
Thies Edeling <thies@te-con.nl>
2013-01-16 00:31:58 +0100

    modified to changes in IHeaderContributor

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/chart/HighChartContainer.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/chart/HighChartContainer.scala
@@ -1,15 +1,16 @@
 package net.rrm.ehour.ui.chart
 
 import org.apache.wicket.markup.html.panel.Panel
-import org.apache.wicket.markup.html.{IHeaderResponse, IHeaderContributor}
+import org.apache.wicket.markup.html.IHeaderContributor
 import net.rrm.ehour.report.reports.ReportData
 import org.apache.wicket.model.IModel
 import net.rrm.ehour.ui.common.session.EhourWebSession
-import net.rrm.ehour.config.EhourConfig
+import org.apache.wicket.markup.head.{OnLoadHeaderItem, IHeaderResponse}
+import net.rrm.ehour.ui.report.panel.aggregate.ChartContext
 
-class HighChartContainer(id: String, reportModel: IModel[ReportData], generateChart: (String, ReportData, EhourConfig) => String) extends Panel(id, reportModel) with IHeaderContributor {
+class HighChartContainer(id: String, reportModel: IModel[ReportData], generateChart: (ChartContext) => String) extends Panel(id, reportModel) with IHeaderContributor {
   setOutputMarkupId(true)
-  
+
   override def renderHead(response: IHeaderResponse) {
     val session = EhourWebSession.getSession
     val config = session.getEhourConfig
@@ -17,6 +18,6 @@ class HighChartContainer(id: String, reportModel: IModel[ReportData], generateCh
     val chart = generateChart(ChartContext(getMarkupId, getDefaultModelObject.asInstanceOf[ReportData], config.getCurrencySymbol, session.isWithReportRole))
     val javascript = "new Highcharts.Chart({%s});\n" format chart
 
-    response.renderOnLoadJavaScript(javascript)
+    response.render(new OnLoadHeaderItem(javascript))
   }
 }
