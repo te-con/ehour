e707b8bf9071c0b4b140b058e6e958ac536b503a
Thies Edeling <thies@te-con.nl>
2013-01-20 14:30:12 +0100

    RequestCycle moved

--- eHour-audit/src/main/java/net/rrm/ehour/audit/aspect/AuditAspect.java
+++ eHour-audit/src/main/java/net/rrm/ehour/audit/aspect/AuditAspect.java
@@ -26,7 +26,9 @@ import net.rrm.ehour.domain.AuditActionType;
 import net.rrm.ehour.domain.AuditType;
 import net.rrm.ehour.domain.User;
 import net.rrm.ehour.ui.common.session.EhourWebSession;
-import org.apache.wicket.RequestCycle;
+import org.apache.wicket.request.component.IRequestablePage;
+import org.apache.wicket.request.cycle.PageRequestHandlerTracker;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -40,272 +42,243 @@ import java.util.Date;
 
 /**
  * Auditable Aspect
- **/
+ */
 @Aspect
 @Component
-public class AuditAspect
-{
-	@Autowired
-	private AuditService	auditService;
-	
-	@Autowired
-	private ConfigurationService configurationService;
+public class AuditAspect {
+    @Autowired
+    private AuditService auditService;
 
-	@Pointcut("execution(public * net.rrm.ehour.*.service.*Service*.get*(..)) && " +
-			"!@annotation(net.rrm.ehour.audit.annot.Auditable) && " +
-			"!@annotation(net.rrm.ehour.audit.annot.NonAuditable)")
-	public void publicGetMethod()
-	{
-	}
-	
-	@Pointcut("execution(public * net.rrm.ehour.*.service.*Service.persist*(..)) && " +
-			"!@annotation(net.rrm.ehour.audit.annot.Auditable) && " +
-			"!@annotation(net.rrm.ehour.audit.annot.NonAuditable)")
-	public void publicPersistMethod()
-	{
-	}
-	
-	@Pointcut("execution(public * net.rrm.ehour.*.service.*Service.delete*(..)) && " +
-			"!@annotation(net.rrm.ehour.audit.annot.Auditable) && " +
-			"!@annotation(net.rrm.ehour.audit.annot.NonAuditable)")
-	public void publicDeleteMethod()
-	{
-	}	
-	
-	/**
-	 * Around advise for read-only get methods
-	 * @param pjp
-	 * @return
-	 * @throws Throwable
-	 */
-	@Around("net.rrm.ehour.audit.aspect.AuditAspect.publicGetMethod()")
-	public Object auditOnGet(ProceedingJoinPoint pjp) throws Throwable
-	{
-		return doAudit(pjp, AuditActionType.READ);
-	}
-	
-	/**
-	 * Around advise for persist methods
-	 * @param pjp
-	 * @return
-	 * @throws Throwable
-	 */
-	@Around("net.rrm.ehour.audit.aspect.AuditAspect.publicPersistMethod()")
-	public Object auditOnPersist(ProceedingJoinPoint pjp) throws Throwable
-	{
-		return doAudit(pjp, AuditActionType.UPDATE);
-	}	
+    @Autowired
+    private ConfigurationService configurationService;
 
-	/**
-	 * Around advise for delete methods
-	 * @param pjp
-	 * @return
-	 * @throws Throwable
-	 */
-	@Around("net.rrm.ehour.audit.aspect.AuditAspect.publicDeleteMethod()")
-	public Object auditOnDelete(ProceedingJoinPoint pjp) throws Throwable
-	{
-		return doAudit(pjp, AuditActionType.DELETE);
-	}	
+    @Pointcut("execution(public * net.rrm.ehour.*.service.*Service*.get*(..)) && " +
+            "!@annotation(net.rrm.ehour.audit.annot.Auditable) && " +
+            "!@annotation(net.rrm.ehour.audit.annot.NonAuditable)")
+    public void publicGetMethod() {
+    }
 
-	/**
-	 * Audit 
-	 * @param pjp
-	 * @param auditable
-	 * @return
-	 * @throws Throwable
-	 */
-	@Around("@annotation(auditable)")
-	public Object doAudit(ProceedingJoinPoint pjp, Auditable auditable) throws Throwable
-	{	
-		return doAudit(pjp, auditable.actionType());
-	}
-	
-	/**
-	 * 
-	 * @param pjp
-	 * @param auditActionType
-	 * @return
-	 * @throws Throwable
-	 */
-	private Object doAudit(ProceedingJoinPoint pjp, AuditActionType auditActionType) throws Throwable
-	{
-		Object returnObject;
-		
-		boolean isAuditable = isAuditable(pjp) && isAuditEnabled(auditActionType);
+    @Pointcut("execution(public * net.rrm.ehour.*.service.*Service.persist*(..)) && " +
+            "!@annotation(net.rrm.ehour.audit.annot.Auditable) && " +
+            "!@annotation(net.rrm.ehour.audit.annot.NonAuditable)")
+    public void publicPersistMethod() {
+    }
 
-		User user = getUser();
-		
-		try
-		{
-			returnObject = pjp.proceed();
-		}
-		catch (Exception t)
-		{
-			if (isAuditable)
-			{
-				auditService.doAudit(createAudit(user, Boolean.FALSE, auditActionType, pjp));
-			}
-			
-			throw t;
-		}
-		
-		if (isAuditable)
-		{
-			auditService.doAudit(createAudit(user, Boolean.TRUE, auditActionType, pjp));
-		}
-		
+    @Pointcut("execution(public * net.rrm.ehour.*.service.*Service.delete*(..)) && " +
+            "!@annotation(net.rrm.ehour.audit.annot.Auditable) && " +
+            "!@annotation(net.rrm.ehour.audit.annot.NonAuditable)")
+    public void publicDeleteMethod() {
+    }
+
+    /**
+     * Around advise for read-only get methods
+     *
+     * @param pjp
+     * @return
+     * @throws Throwable
+     */
+    @Around("net.rrm.ehour.audit.aspect.AuditAspect.publicGetMethod()")
+    public Object auditOnGet(ProceedingJoinPoint pjp) throws Throwable {
+        return doAudit(pjp, AuditActionType.READ);
+    }
+
+    /**
+     * Around advise for persist methods
+     *
+     * @param pjp
+     * @return
+     * @throws Throwable
+     */
+    @Around("net.rrm.ehour.audit.aspect.AuditAspect.publicPersistMethod()")
+    public Object auditOnPersist(ProceedingJoinPoint pjp) throws Throwable {
+        return doAudit(pjp, AuditActionType.UPDATE);
+    }
+
+    /**
+     * Around advise for delete methods
+     *
+     * @param pjp
+     * @return
+     * @throws Throwable
+     */
+    @Around("net.rrm.ehour.audit.aspect.AuditAspect.publicDeleteMethod()")
+    public Object auditOnDelete(ProceedingJoinPoint pjp) throws Throwable {
+        return doAudit(pjp, AuditActionType.DELETE);
+    }
+
+    /**
+     * Audit
+     *
+     * @param pjp
+     * @param auditable
+     * @return
+     * @throws Throwable
+     */
+    @Around("@annotation(auditable)")
+    public Object doAudit(ProceedingJoinPoint pjp, Auditable auditable) throws Throwable {
+        return doAudit(pjp, auditable.actionType());
+    }
+
+    /**
+     * @param pjp
+     * @param auditActionType
+     * @return
+     * @throws Throwable
+     */
+    private Object doAudit(ProceedingJoinPoint pjp, AuditActionType auditActionType) throws Throwable {
+        Object returnObject;
+
+        boolean isAuditable = isAuditable(pjp) && isAuditEnabled(auditActionType);
+
+        User user = getUser();
+
+        try {
+            returnObject = pjp.proceed();
+        } catch (Exception t) {
+            if (isAuditable) {
+                auditService.doAudit(createAudit(user, Boolean.FALSE, auditActionType, pjp));
+            }
+
+            throw t;
+        }
+
+        if (isAuditable) {
+            auditService.doAudit(createAudit(user, Boolean.TRUE, auditActionType, pjp));
+        }
+
 //		LOGGER.debug("exiting audit");
-		
-		return returnObject;		
-	}
-	
-	/**
-	 * Is audit type enabled
-	 * @param actionType
-	 * @return
-	 */
-	private boolean isAuditEnabled(AuditActionType actionType)
-	{
-		EhourConfig config = configurationService.getConfiguration();
 
-		if (config.getAuditType() == AuditType.NONE)
-		{
-			return false;
-		}
-		
-		if (config.getAuditType() == AuditType.WRITE &&
-				actionType.getAuditType() == config.getAuditType())
-		{
-			return true;
-		}
+        return returnObject;
+    }
+
+    /**
+     * Is audit type enabled
+     *
+     * @param actionType
+     * @return
+     */
+    private boolean isAuditEnabled(AuditActionType actionType) {
+        EhourConfig config = configurationService.getConfiguration();
+
+        if (config.getAuditType() == AuditType.NONE) {
+            return false;
+        }
+
+        if (config.getAuditType() == AuditType.WRITE &&
+                actionType.getAuditType() == config.getAuditType()) {
+            return true;
+        }
 
         return config.getAuditType() == AuditType.ALL;
 
     }
-	
-	/**
-	 * Check if type is annotated with NonAuditable annotation
-	 * @param pjp
-	 * @return
-	 */
-	private boolean isAuditable(ProceedingJoinPoint pjp)
-	{
-		Annotation[] annotations = pjp.getSignature().getDeclaringType().getAnnotations();
 
-		boolean nonAuditable = false;
-		
-		for (Annotation annotation : annotations)
-		{
-			nonAuditable = annotation.annotationType() == NonAuditable.class;
-			
-			if (nonAuditable)
-			{
-				break;
-			}
-		}
-		
-		return !nonAuditable;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	private User getUser()
-	{
-		User user;
-		
-		try
-		{
-			user = EhourWebSession.getSession().getUser().getUser();
-		}
-		catch (Exception t)
-		{
-			user = null;
-		}
-		
-		return user;
-	}
+    /**
+     * Check if type is annotated with NonAuditable annotation
+     *
+     * @param pjp
+     * @return
+     */
+    private boolean isAuditable(ProceedingJoinPoint pjp) {
+        Annotation[] annotations = pjp.getSignature().getDeclaringType().getAnnotations();
 
-	/**
-	 * 
-	 * @param user
-	 * @param success
-	 * @param action
-	 * @param pjp
-	 * @return
-	 */
-	private Audit createAudit(User user, Boolean success, AuditActionType auditActionType, ProceedingJoinPoint pjp)
-	{
-		String parameters = getAuditParameters(pjp);
-		
-		String page = null;
-		
-		if (RequestCycle.get() != null && RequestCycle.get().getResponsePage() != null)
-		{
-			page = RequestCycle.get().getResponsePage().getClass().getCanonicalName();
-		}
-		
-		return new Audit()
-				.setUser(user)
-				.setUserFullName(user != null ? user.getFullName() : null)
-				.setDate(new Date())
-				.setSuccess(success)
-				.setAction(pjp.getSignature().toShortString())
-				.setAuditActionType(auditActionType)
-				.setParameters(parameters)
-				.setPage(page)
-				;
-	}
-	
-	/**
-	 * Get parameters of advised method
-	 * @param pjp
-	 * @return
-	 */
-	private String getAuditParameters(ProceedingJoinPoint pjp)
-	{
-		StringBuilder parameters = new StringBuilder();
-		
-		int i = 0;
-		
-		for (Object object : pjp.getArgs())
-		{
-			parameters.append(i++ + ":");
-	
-			if (object == null)
-			{
-				parameters.append("null");
-			} else if (object instanceof Calendar)
-			{
-				parameters.append(((Calendar)object).getTime().toString());
-			}
-			else
-			{
-				parameters.append(object.toString());
-			}
-		}		
-		
-		
-		
-		return parameters.length() > 1024 ? parameters.substring(0, 1023) : parameters.toString();
-	}
+        boolean nonAuditable = false;
 
-	
-	/**
-	 * @param auditService the auditService to set
-	 */
-	public void setAuditService(AuditService auditService)
-	{
-		this.auditService = auditService;
-	}
+        for (Annotation annotation : annotations) {
+            nonAuditable = annotation.annotationType() == NonAuditable.class;
 
-	/**
-	 * @param configurationService the configurationService to set
-	 */
-	public void setConfigurationService(ConfigurationService configurationService)
-	{
-		this.configurationService = configurationService;
-	}
+            if (nonAuditable) {
+                break;
+            }
+        }
+
+        return !nonAuditable;
+    }
+
+    /**
+     * @return
+     */
+    private User getUser() {
+        User user;
+
+        try {
+            user = EhourWebSession.getSession().getUser().getUser();
+        } catch (Exception t) {
+            user = null;
+        }
+
+        return user;
+    }
+
+    /**
+     * @param user
+     * @param success
+     * @param action
+     * @param pjp
+     * @return
+     */
+    private Audit createAudit(User user, Boolean success, AuditActionType auditActionType, ProceedingJoinPoint pjp) {
+        String parameters = getAuditParameters(pjp);
+
+        String page = null;
+
+        IRequestablePage responsePage = PageRequestHandlerTracker.getLastHandler(RequestCycle.get()).getPage();
+
+        if (responsePage != null) {
+            page = responsePage.getClass().getCanonicalName();
+        }
+
+        return new Audit()
+                .setUser(user)
+                .setUserFullName(user != null ? user.getFullName() : null)
+                .setDate(new Date())
+                .setSuccess(success)
+                .setAction(pjp.getSignature().toShortString())
+                .setAuditActionType(auditActionType)
+                .setParameters(parameters)
+                .setPage(page)
+                ;
+    }
+
+    /**
+     * Get parameters of advised method
+     *
+     * @param pjp
+     * @return
+     */
+    private String getAuditParameters(ProceedingJoinPoint pjp) {
+        StringBuilder parameters = new StringBuilder();
+
+        int i = 0;
+
+        for (Object object : pjp.getArgs()) {
+            parameters.append(i++ + ":");
+
+            if (object == null) {
+                parameters.append("null");
+            } else if (object instanceof Calendar) {
+                parameters.append(((Calendar) object).getTime().toString());
+            } else {
+                parameters.append(object.toString());
+            }
+        }
+
+
+        return parameters.length() > 1024 ? parameters.substring(0, 1023) : parameters.toString();
+    }
+
+
+    /**
+     * @param auditService the auditService to set
+     */
+    public void setAuditService(AuditService auditService) {
+        this.auditService = auditService;
+    }
+
+    /**
+     * @param configurationService the configurationService to set
+     */
+    public void setConfigurationService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
 }
