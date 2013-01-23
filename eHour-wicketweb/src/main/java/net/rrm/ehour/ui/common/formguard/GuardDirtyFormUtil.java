8a004a5c7b1175d55297ac12258a2af9d1b952b5
Thies Edeling <thies@te-con.nl>
2013-01-23 21:43:33 +0100

    typo in the js

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/common/formguard/GuardDirtyFormUtil.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/common/formguard/GuardDirtyFormUtil.java
@@ -1,7 +1,7 @@
 package net.rrm.ehour.ui.common.formguard;
 
 public final class GuardDirtyFormUtil {
-    public static final String PRECONDITION = "var k=true;if (window.wicket && wicket.guardform) { k=wicket.guardform.promptForDirtyForm(); };reurn (k);";
+    public static final String PRECONDITION = "var k=true;if (window.wicket && wicket.guardform) { k=wicket.guardform.promptForDirtyForm(); };return (k);";
 
     private GuardDirtyFormUtil() {
     }
