eb4ce92514daa369d464cb52f66ee3a33253349a
Thies Edeling <thies@te-con.nl>
2013-05-29 15:22:54 +0200

    EHO-327 dummy dataload scripts added

---
+++ bench/delete.sql
@@ -0,0 +1,13 @@
+DELETE FROM MAIL_LOG;
+DELETE FROM project_assignment;
+
+DELETE FROM project;
+
+DELETE FROM customer;
+DELETE FROM user_to_userrole;
+
+DELETE FROM users;
+
+DELETE FROM user_department;
+
+DELETE FROM TIMESHEET_ENTRY;
