16160143fd0c4cf167f761e24367c20440a543b4
Thies Edeling <thies@te-con.nl>
2013-06-04 14:23:40 +0200

    EHO-330 should move to customize

--- eHour-selenium/src/test/java/net/rrm/ehour/it/scenario/admin/ConfigCustomizeScenario.java
+++ eHour-selenium/src/test/java/net/rrm/ehour/it/scenario/admin/ConfigCustomizeScenario.java
@@ -2,11 +2,17 @@ package net.rrm.ehour.it.scenario.admin;
 
 import net.rrm.ehour.it.AbstractScenario;
 import net.rrm.ehour.it.ScenarioHelper;
+import net.rrm.ehour.it.WicketBy;
 import org.junit.Test;
 
 public class ConfigCustomizeScenario extends AbstractScenario {
     @Test
     public void should_upload_excel() {
         ScenarioHelper.Login(Driver, baseUrl);
+
+        Driver.findElement(WicketBy.wicketPath("configTabs_tabs-container_tabs_3_link_title")).click();
+
+        // Warning: verifyTextPresent may require manual changes
+//            assertTrue(Driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Upload a replacement logo[\\s\\S]*$"));
     }
 }
