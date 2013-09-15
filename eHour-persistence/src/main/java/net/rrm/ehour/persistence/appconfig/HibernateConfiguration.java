d4251396ceca1a13d2001b4ffee27616bb70ce34
Thies Edeling <thies@te-con.nl>
2013-09-15 23:43:07 +0200

    Hibernate's query cache is disabled while running integration tests

--- eHour-persistence/src/main/java/net/rrm/ehour/persistence/appconfig/HibernateConfiguration.java
+++ eHour-persistence/src/main/java/net/rrm/ehour/persistence/appconfig/HibernateConfiguration.java
@@ -24,85 +24,88 @@ import java.util.List;
 import java.util.Properties;
 
 @Configuration
-public class HibernateConfiguration
-{
-	@Autowired
-	private DataSource dataSource;
+public class HibernateConfiguration {
+    @Autowired
+    private DataSource dataSource;
 
-	@Value("${ehour.database}")
-	private String databaseName;
+    @Value("${ehour.database}")
+    private String databaseName;
 
-	private static final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-	private static final Logger LOGGER = Logger.getLogger(HibernateConfiguration.class);
+    @Value("${ehour.db.cache:true}")
+    private String caching;
 
-	@Bean(name="sessionFactory")
-	public SessionFactory getSessionFactory() throws Exception
-	{
-		Properties configProperties = EhourHomeUtil.loadDatabaseProperties(databaseName);
+    private static final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+    private static final Logger LOGGER = Logger.getLogger(HibernateConfiguration.class);
 
-		LOGGER.info("Using database type: " + databaseName);
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory() throws Exception {
+        if (!caching.equalsIgnoreCase("true") && !caching.equalsIgnoreCase("false")) {
+            throw new IllegalArgumentException("ehour.db.cache property must either be true or false");
+        }
+
+        Properties configProperties = EhourHomeUtil.loadDatabaseProperties(databaseName);
+
+        LOGGER.info("Using database type: " + databaseName);
 
         AnnotationSessionFactoryBean factoryBean = new AnnotationSessionFactoryBean();
-		factoryBean.setDataSource(dataSource);
+        factoryBean.setDataSource(dataSource);
 
-		List<Resource> resources = getMappingResources(configProperties);
-		factoryBean.setMappingLocations(resources.toArray(new Resource[resources.size()]));
+        List<Resource> resources = getMappingResources(configProperties);
+        factoryBean.setMappingLocations(resources.toArray(new Resource[resources.size()]));
         factoryBean.setAnnotatedClasses(DomainObjects.DOMAIN_OBJECTS);
 
-		Properties hibernateProperties = new Properties();
-		hibernateProperties.put("hibernate.dialect", configProperties.get("hibernate.dialect"));
-		hibernateProperties.put("show_sql", "false");
-		hibernateProperties.put("use_outer_join", "true");
-		hibernateProperties.put("hibernate.cache.region.factory_class", "net.sf.ehcache.hibernate.EhCacheRegionFactory");
-		hibernateProperties.put("hibernate.cache.use_second_level_cache", "true");
-		hibernateProperties.put("net.sf.ehcache.configurationResourceName", "ehcache.xml");
-		hibernateProperties.put("hibernate.cache.use_query_cache", "true");
-		hibernateProperties.put("hibernate.hbm2ddl.auto", configProperties.get("hibernate.hbm2ddl.auto"));
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", configProperties.get("hibernate.dialect"));
+        hibernateProperties.put("show_sql", "false");
+        hibernateProperties.put("use_outer_join", "true");
+        hibernateProperties.put("hibernate.cache.region.factory_class", "net.sf.ehcache.hibernate.EhCacheRegionFactory");
+        hibernateProperties.put("hibernate.cache.use_second_level_cache", caching);
+        hibernateProperties.put("net.sf.ehcache.configurationResourceName", "ehcache.xml");
+        hibernateProperties.put("hibernate.cache.use_query_cache", caching);
+        hibernateProperties.put("hibernate.hbm2ddl.auto", configProperties.get("hibernate.hbm2ddl.auto"));
 
-		factoryBean.setHibernateProperties(hibernateProperties);
-		factoryBean.afterPropertiesSet();
+        factoryBean.setHibernateProperties(hibernateProperties);
+        factoryBean.afterPropertiesSet();
 
         return factoryBean.getObject();
-	}
+    }
 
-	private List<Resource> getMappingResources(Properties configProperties) throws IOException
-	{
-		List<Resource> resources = new ArrayList<Resource>();
+    private List<Resource> getMappingResources(Properties configProperties) throws IOException {
+        List<Resource> resources = new ArrayList<Resource>();
 
-		Resource[] queryResources = resolver.getResources("classpath:query/common/*.hbm.xml");
-		resources.addAll(Arrays.asList(queryResources));
+        Resource[] queryResources = resolver.getResources("classpath:query/common/*.hbm.xml");
+        resources.addAll(Arrays.asList(queryResources));
 
-		ClassPathResource dbQueryResource = new ClassPathResource("query/" + configProperties.getProperty("reportquery.filename"));
-		resources.add(dbQueryResource);
-		return resources;
-	}
+        ClassPathResource dbQueryResource = new ClassPathResource("query/" + configProperties.getProperty("reportquery.filename"));
+        resources.add(dbQueryResource);
+        return resources;
+    }
 
-	@Bean(name="transactionManager")
-	public  HibernateTransactionManager getTransactionManager() throws Exception
-	{
-		return new HibernateTransactionManager(getSessionFactory());
-	}
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager() throws Exception {
+        return new HibernateTransactionManager(getSessionFactory());
+    }
 
-	public @Bean HibernateTemplate getHibernateTemplate() throws Exception
-	{
-		return new HibernateTemplate(getSessionFactory());
-	}
+    public
+    @Bean
+    HibernateTemplate getHibernateTemplate() throws Exception {
+        return new HibernateTemplate(getSessionFactory());
+    }
 
-	public @Bean JdbcTemplate getJdbcTemplate() throws Exception
-	{
-		return new JdbcTemplate(dataSource);
-	}
+    public
+    @Bean
+    JdbcTemplate getJdbcTemplate() throws Exception {
+        return new JdbcTemplate(dataSource);
+    }
 
-	/**
-	 * @param databaseName the databaseName to set
-	 */
-	void setDatabaseName(String databaseName)
-	{
-		this.databaseName = databaseName;
-	}
+    /**
+     * @param databaseName the databaseName to set
+     */
+    void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
 
-    public void setDataSource(DataSource dataSource)
-    {
+    public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 }
