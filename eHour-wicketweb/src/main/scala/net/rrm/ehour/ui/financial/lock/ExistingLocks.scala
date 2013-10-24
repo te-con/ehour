0007fc4512e7d0f2f737610965bcb2efd1462514
Thies Edeling <thies@te-con.nl>
2013-10-25 01:36:54 +0200

    EHO-62 fixed layout

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/ExistingLocks.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/ExistingLocks.scala
@@ -1,14 +1,13 @@
 package net.rrm.ehour.ui.financial.lock
 
 import net.rrm.ehour.ui.common.panel.AbstractAjaxPanel
-import net.rrm.ehour.ui.common.border.GreyBlueRoundedBorder
+import net.rrm.ehour.ui.common.border.GreyRoundedBorder
 
 class ExistingLocksPanel(id: String) extends AbstractAjaxPanel[Unit](id) {
   override def onInitialize() {
     super.onInitialize()
 
-
-    val greyBorder = new GreyBlueRoundedBorder(ExistingLocksPanel.BorderId)
+    val greyBorder = new GreyRoundedBorder(ExistingLocksPanel.BorderId, "Existing locks")
     add(greyBorder)
   }
 }
--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockDetailsPanel.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockDetailsPanel.scala
@@ -1,17 +1,21 @@
 package net.rrm.ehour.ui.financial.lock
 
 import net.rrm.ehour.ui.common.panel.AbstractAjaxPanel
-import net.rrm.ehour.ui.common.border.GreyRoundedBorder
+import net.rrm.ehour.ui.common.border.{GreyBlueRoundedBorder, GreyRoundedBorder}
 
 class LockDetailsPanel(id: String) extends AbstractAjaxPanel[Unit](id) {
   override def onInitialize() {
     super.onInitialize()
 
-    val greyBorder = new GreyRoundedBorder(LockDetailsPanel.BorderId)
-    add(greyBorder)
+    val outerBorder = new GreyRoundedBorder(LockDetailsPanel.OuterBorderId, "New")
+    add(outerBorder)
+
+    val innerBorder = new GreyBlueRoundedBorder(LockDetailsPanel.InnerBorderId)
+    outerBorder.add(innerBorder)
   }
 }
 
 object LockDetailsPanel {
-  val BorderId = "border"
+  val OuterBorderId = "outerBorder"
+  val InnerBorderId = "innerBorder"
 }
