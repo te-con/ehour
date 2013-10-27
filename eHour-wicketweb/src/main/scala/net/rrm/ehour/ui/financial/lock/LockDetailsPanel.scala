9df3f3f32fd1c1a7f535c2b4a1856e68eef573b8
Thies Edeling <thies@te-con.nl>
2013-10-27 23:45:49 +0100

    EHO-62 Lock form added (layout only)

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockDetailsPanel.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockDetailsPanel.scala
@@ -1,17 +1,17 @@
 package net.rrm.ehour.ui.financial.lock
 
 import net.rrm.ehour.ui.common.panel.AbstractAjaxPanel
-import net.rrm.ehour.ui.common.border.{GreyBlueRoundedBorder, GreyRoundedBorder}
+import net.rrm.ehour.ui.common.border.GreyRoundedBorder
 
 class LockDetailsPanel(id: String) extends AbstractAjaxPanel[Unit](id) {
   override def onInitialize() {
     super.onInitialize()
 
-    val outerBorder = new GreyRoundedBorder(LockDetailsPanel.OuterBorderId, "New")
+    val outerBorder = new GreyRoundedBorder(LockDetailsPanel.OuterBorderId, "Timesheet lock details")
     add(outerBorder)
-
-    val innerBorder = new GreyBlueRoundedBorder(LockDetailsPanel.InnerBorderId)
-    outerBorder.add(innerBorder)
+//
+//    val innerBorder = new GreyBlueRoundedBorder(LockDetailsPanel.InnerBorderId)
+//    outerBorder.add(innerBorder)
   }
 }
 
