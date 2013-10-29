c58eb057ef81e2e62f76f3f018c76987cd6e63e3
Thies Edeling <thies@te-con.nl>
2013-10-29 21:50:36 +0100

    EHO-62 lockservice was not mocked

--- eHour-wicketweb/src/test/scala/net/rrm/ehour/ui/financial/lock/LockAdminPageSpec.scala
+++ eHour-wicketweb/src/test/scala/net/rrm/ehour/ui/financial/lock/LockAdminPageSpec.scala
@@ -1,11 +1,18 @@
 package net.rrm.ehour.ui.financial.lock
 
 import net.rrm.ehour.AbstractSpringWebAppSpec
+import net.rrm.ehour.timesheet.service.TimesheetLockService
 
 class LockAdminPageSpec extends AbstractSpringWebAppSpec {
   "Lock Admin Page" should {
+    val service = mock[TimesheetLockService]
+    springTester.getMockContext.putBean(service)
+
     "render" in {
       tester.startPage(classOf[LockAdminPage])
+
+      tester.assertNoErrorMessage()
+      tester.assertNoInfoMessage()
     }
   }
 }
