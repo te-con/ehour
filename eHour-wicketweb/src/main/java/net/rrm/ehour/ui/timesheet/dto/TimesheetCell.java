8f0c4e20caa5de76bab21a991e51cfee577125d4
Thies Edeling <thies@te-con.nl>
2013-11-03 02:38:48 +0100

    reformat

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/timesheet/dto/TimesheetCell.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/timesheet/dto/TimesheetCell.java
@@ -16,68 +16,65 @@
 
 package net.rrm.ehour.ui.timesheet.dto;
 
+import net.rrm.ehour.domain.TimesheetEntry;
+
 import java.io.Serializable;
 import java.util.Date;
 
-import net.rrm.ehour.domain.TimesheetEntry;
-
 /**
  * Representation of a cell (day in a project) in the timesheet
- **/
+ */
 
-public class TimesheetCell implements Comparable<TimesheetCell>, Serializable
-{
-	private static final long serialVersionUID = -2708559856313387714L;
-	private TimesheetEntry	timesheetEntry;
-	private	boolean			isValid;
-	private Date			date;
+public class TimesheetCell implements Comparable<TimesheetCell>, Serializable {
+    private static final long serialVersionUID = -2708559856313387714L;
+    private TimesheetEntry timesheetEntry;
+    private boolean isValid;
+    private Date date;
 
-	/**
-	 * @return the isValid
-	 */
-	public boolean isValid()
-	{
-		return isValid;
-	}
-	/**
-	 * @param isValid the isValid to set
-	 */
-	public void setValid(boolean isValid)
-	{
-		this.isValid = isValid;
-	}
-	/**
-	 * @return the timesheetEntry
-	 */
-	public TimesheetEntry getTimesheetEntry()
-	{
-		return timesheetEntry;
-	}
-	/**
-	 * @param timesheetEntry the timesheetEntry to set
-	 */
-	public void setTimesheetEntry(TimesheetEntry timesheetEntry)
-	{
-		this.timesheetEntry = timesheetEntry;
-	}
-	
-	
-	public int compareTo(TimesheetCell o)
-	{
-		return getTimesheetEntry().getEntryId().getEntryDate().compareTo(o.getTimesheetEntry().getEntryId().getEntryDate());
-	}
-	/**
-	 * @return the date
-	 */
-	public Date getDate()
-	{
-		return date;
-	}
-	/**
-	 * @param date the date to set
-	 */
-	public void setDate(Date date)
-	{
-		this.date = date;
-	}
+    /**
+     * @return the isValid
+     */
+    public boolean isValid() {
+        return isValid;
+    }
+
+    /**
+     * @param isValid the isValid to set
+     */
+    public void setValid(boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    /**
+     * @return the timesheetEntry
+     */
+    public TimesheetEntry getTimesheetEntry() {
+        return timesheetEntry;
+    }
+
+    /**
+     * @param timesheetEntry the timesheetEntry to set
+     */
+    public void setTimesheetEntry(TimesheetEntry timesheetEntry) {
+        this.timesheetEntry = timesheetEntry;
+    }
+
+
+    public int compareTo(TimesheetCell o) {
+        return getTimesheetEntry().getEntryId().getEntryDate().compareTo(o.getTimesheetEntry().getEntryId().getEntryDate());
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
