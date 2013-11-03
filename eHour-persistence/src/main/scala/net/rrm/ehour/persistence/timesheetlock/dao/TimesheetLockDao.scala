177a89c683d934b9240ba1189895de3474957d9a
Thies Edeling <thies@te-con.nl>
2013-11-03 03:10:30 +0100

    EHO-62 ability to find any locks for a given date range

--- eHour-persistence/src/main/scala/net/rrm/ehour/persistence/timesheetlock/dao/TimesheetLockDao.scala
+++ eHour-persistence/src/main/scala/net/rrm/ehour/persistence/timesheetlock/dao/TimesheetLockDao.scala
@@ -3,12 +3,19 @@ package net.rrm.ehour.persistence.timesheetlock.dao
 import net.rrm.ehour.persistence.dao.{AbstractGenericDaoHibernateImpl, GenericDao}
 import net.rrm.ehour.domain.TimesheetLock
 import org.springframework.stereotype.Repository
+import java.util.Date
+import java.util.{List => JList}
 
 trait TimesheetLockDao extends GenericDao[TimesheetLock, Integer] {
+  def findMatchingLock(start: Date, end: Date): JList[TimesheetLock]
 }
 
 @Repository("timesheetLockDao")
 class TimesheetLockHibernateImpl extends AbstractGenericDaoHibernateImpl[TimesheetLock, Integer](classOf[TimesheetLock]) with TimesheetLockDao {
+  def findMatchingLock(start: Date, end: Date): JList[TimesheetLock] = {
+    val keys = Array[String]("start", "end")
+    val params = Array[AnyRef](start, end)
 
+    getHibernateTemplate.findByNamedQueryAndNamedParam("TimesheetLock.getLocksMatchingDate", keys, params).asInstanceOf[JList[TimesheetLock]]
+  }
 }
-
--- eHour-persistence/src/test/groovy/net/rrm/ehour/persistence/timesheetlock/dao/TimesheetLockHibernateImplTest.groovy
+++ eHour-persistence/src/test/groovy/net/rrm/ehour/persistence/timesheetlock/dao/TimesheetLockHibernateImplTest.groovy
@@ -2,19 +2,22 @@ package net.rrm.ehour.persistence.timesheetlock.dao
 
 import net.rrm.ehour.domain.TimesheetLock
 import net.rrm.ehour.persistence.dao.AbstractAnnotationDaoTest
+import org.joda.time.DateTime
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
-import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.*
 
-class TimesheetLockHibernateImplTest extends AbstractAnnotationDaoTest{
+class TimesheetLockHibernateImplTest extends AbstractAnnotationDaoTest {
     @Autowired
     private TimesheetLockDao timesheetLockDao;
 
+    TimesheetLockHibernateImplTest() {
+        super("dataset-timesheetlock.xml")
+    }
+
     @Test
-    void shouldPersist()
-    {
+    void shouldPersist() {
         def startDate = new Date()
         def timesheetLock = new TimesheetLock(startDate, new Date())
         def id = timesheetLockDao.persist(timesheetLock)
@@ -23,4 +26,31 @@ class TimesheetLockHibernateImplTest extends AbstractAnnotationDaoTest{
         def persistedLock = timesheetLockDao.findById(id.lockId)
         assertEquals(startDate, persistedLock.dateStart)
     }
+
+    @Test
+    void shouldFindMatching() {
+        def start = new DateTime(2013, 11, 3, 0, 0, 0, 0)
+        def locks = timesheetLockDao.findMatchingLock(start.plusDays(1).toDate(), start.plusDays(3).toDate())
+
+        assertEquals(1, locks[0].lockId)
+    }
+
+    @Test
+    void shouldNotFindMatchingBefore() {
+        def start = new DateTime(2013, 10, 3, 0, 0, 0, 0)
+        def locks = timesheetLockDao.findMatchingLock(start.plusDays(1).toDate(), start.plusDays(3).toDate())
+
+        assertTrue(locks.isEmpty())
+    }
+
+    @Test
+    void shouldNotFindMatchingAfter() {
+        def start = new DateTime(2013, 12, 3, 0, 0, 0, 0)
+        def locks = timesheetLockDao.findMatchingLock(start.plusDays(1).toDate(), start.plusDays(3).toDate())
+
+        assertTrue(locks.isEmpty())
+    }
+
+
+
 }
