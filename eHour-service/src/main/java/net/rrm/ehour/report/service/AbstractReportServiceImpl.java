e5e982e79a40bd219db34a8b981bee8f70238fc1
Thies Edeling <thies@te-con.nl>
2013-11-19 14:30:26 +0100

    pattern fix

--- eHour-service/src/main/java/net/rrm/ehour/report/service/AbstractReportServiceImpl.java
+++ eHour-service/src/main/java/net/rrm/ehour/report/service/AbstractReportServiceImpl.java
@@ -32,43 +32,40 @@ import java.util.List;
 /**
  * Abstract report service provides utility methods for dealing
  * with the usercriteria obj
- **/
+ */
 
-public abstract class AbstractReportServiceImpl<RE extends ReportElement>
-{
-	@Autowired
-	private	UserDao		userDAO;
+public abstract class AbstractReportServiceImpl<RE extends ReportElement> {
+    @Autowired
+    private UserDao userDAO;
 
-	@Autowired
-	private	ProjectDao	projectDAO;
-	
-	/**
-	 * Get report data for criteria
-	 * @param reportCriteria
-	 * @return
-	 */
-	protected ReportData getReportData(ReportCriteria reportCriteria)
-	{
-		UserSelectedCriteria userSelectedCriteria;
-		List<Project>	projects = null;
-		List<User>		users = null;
-		boolean			ignoreUsers;
-		boolean			ignoreProjects;
-		DateRange		reportRange;
-		
-		userSelectedCriteria = reportCriteria.getUserSelectedCriteria();
+    @Autowired
+    private ProjectDao projectDAO;
 
-		reportRange = reportCriteria.getReportRange();
-		
-		ignoreUsers = userSelectedCriteria.isEmptyDepartments() && userSelectedCriteria.isEmptyUsers();
-		ignoreProjects = userSelectedCriteria.isEmptyCustomers() && userSelectedCriteria.isEmptyProjects();
+    /**
+     * Get report data for criteria
+     *
+     * @param reportCriteria
+     * @return
+     */
+    protected ReportData getReportData(ReportCriteria reportCriteria) {
+        UserSelectedCriteria userSelectedCriteria;
+        List<Project> projects = null;
+        List<User> users = null;
+        boolean ignoreUsers;
+        boolean ignoreProjects;
+        DateRange reportRange;
+
+        userSelectedCriteria = reportCriteria.getUserSelectedCriteria();
+
+        reportRange = reportCriteria.getReportRange();
+
+        ignoreUsers = userSelectedCriteria.isEmptyDepartments() && userSelectedCriteria.isEmptyUsers();
+        ignoreProjects = userSelectedCriteria.isEmptyCustomers() && userSelectedCriteria.isEmptyProjects();
 
         if (!ignoreProjects || !ignoreUsers) {
-            if (ignoreProjects)
-            {
+            if (ignoreProjects) {
                 users = getUsers(userSelectedCriteria);
-            }
-            else if (ignoreUsers) {
+            } else if (ignoreUsers) {
                 projects = getProjects(userSelectedCriteria);
             } else {
                 users = getUsers(userSelectedCriteria);
@@ -77,101 +74,86 @@ public abstract class AbstractReportServiceImpl<RE extends ReportElement>
         }
 
         return new ReportData(getReportElements(users, projects, reportRange), reportRange);
-	}
+    }
 
-	/**
-	 * Get the actual data
-	 * @param users
-	 * @param projects
-	 * @param reportRange
-	 * @return
-	 */
-	protected abstract List<RE> getReportElements(List<User> users,
-													List<Project >projects,
-													DateRange reportRange);
-	
-	/**
-	 * Get project id's based on selected customers
-	 * @param userSelectedCriteria
-	 * @return
-	 */
-	protected List<Project> getProjects(UserSelectedCriteria userSelectedCriteria)
-	{
-		List<Project>	projects;
-		
-		// No projects selected by the user, use any given customer limitation 
-		if (userSelectedCriteria.isEmptyProjects())
-		{
-			if (!userSelectedCriteria.isEmptyCustomers())
-			{
-				projects = projectDAO.findProjectForCustomers(userSelectedCriteria.getCustomers(),
-																userSelectedCriteria.isOnlyActiveProjects());
-			}
-			else
-			{
-				projects = null;
-			}
-		}
-		else
-		{
-			projects = userSelectedCriteria.getProjects();
-		}
-		
-		return projects;
-	}	
-	
-	/**
-	 * Get users based on selected departments
-	 * @param userSelectedCriteria
-	 * @return
-	 */
-	protected List<User> getUsers(UserSelectedCriteria userSelectedCriteria)
-	{
-		List<User>		users;
-		
-		if (userSelectedCriteria.isEmptyUsers())
-		{
-			if (!userSelectedCriteria.isEmptyDepartments())
-			{
-				users = userDAO.findUsersForDepartments(null,
-														userSelectedCriteria.getDepartments(),
-														userSelectedCriteria.isOnlyActiveUsers());
-			}
-			else
-			{
-				users = null;
-			}
-		}
-		else
-		{
-			users = userSelectedCriteria.getUsers();
-		}
-		
-		return users;
-	}
+    /**
+     * Get the actual data
+     *
+     * @param users
+     * @param projects
+     * @param reportRange
+     * @return
+     */
+    protected abstract List<RE> getReportElements(List<User> users,
+                                                  List<Project> projects,
+                                                  DateRange reportRange);
+
+    /**
+     * Get project id's based on selected customers
+     *
+     * @param userSelectedCriteria
+     * @return
+     */
+    protected List<Project> getProjects(UserSelectedCriteria userSelectedCriteria) {
+        List<Project> projects;
+
+        // No projects selected by the user, use any given customer limitation
+        if (userSelectedCriteria.isEmptyProjects()) {
+            if (!userSelectedCriteria.isEmptyCustomers()) {
+                projects = projectDAO.findProjectForCustomers(userSelectedCriteria.getCustomers(),
+                        userSelectedCriteria.isOnlyActiveProjects());
+            } else {
+                projects = null;
+            }
+        } else {
+            projects = userSelectedCriteria.getProjects();
+        }
+
+        return projects;
+    }
+
+    /**
+     * Get users based on selected departments
+     *
+     * @param userSelectedCriteria
+     * @return
+     */
+    protected List<User> getUsers(UserSelectedCriteria userSelectedCriteria) {
+        List<User> users;
+
+        if (userSelectedCriteria.isEmptyUsers()) {
+            if (!userSelectedCriteria.isEmptyDepartments()) {
+                users = userDAO.findUsersForDepartments(userSelectedCriteria.getDepartments(),
+                        userSelectedCriteria.isOnlyActiveUsers());
+            } else {
+                users = null;
+            }
+        } else {
+            users = userSelectedCriteria.getUsers();
+        }
+
+        return users;
+    }
 
 
-	/**
-	 * @param userDAO the userDAO to set
-	 */
-	public void setUserDAO(UserDao userDAO)
-	{
-		this.userDAO = userDAO;
-	}
+    /**
+     * @param userDAO the userDAO to set
+     */
+    public void setUserDAO(UserDao userDAO) {
+        this.userDAO = userDAO;
+    }
 
-	/**
-	 * @param projectDAO the projectDAO to set
-	 */
-	public void setProjectDAO(ProjectDao projectDAO)
-	{
-		this.projectDAO = projectDAO;
-	}
+    /**
+     * @param projectDAO the projectDAO to set
+     */
+    public void setProjectDAO(ProjectDao projectDAO) {
+        this.projectDAO = projectDAO;
+    }
 
-	/**
-	 * @return the projectDAO
-	 */
-	protected ProjectDao getProjectDAO()
-	{
-		return projectDAO;
-	}
+    /**
+     * @return the projectDAO
+     */
+    protected ProjectDao getProjectDAO() {
+        return projectDAO;
+    }
 }
