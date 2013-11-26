155438391030282d081674bbc0e69c8a9512152e
Thies Edeling <thies@te-con.nl>
2013-11-26 14:09:50 +0100

    EHO-206 editing existing assignments added

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/common/wicket/WicketDSL.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/common/wicket/WicketDSL.scala
@@ -2,7 +2,7 @@ package net.rrm.ehour.ui.common.wicket
 
 import org.apache.wicket.ajax.markup.html.form.{AjaxButton => WicketAjaxButton}
 import org.apache.wicket.markup.html.form.Form
-import org.apache.wicket.ajax.AjaxRequestTarget
+import org.apache.wicket.ajax.{AjaxEventBehavior, AjaxRequestTarget}
 import net.rrm.ehour.ui.common.wicket.AjaxButton.Callback
 import org.apache.wicket.ajax.markup.html.{AjaxLink => WicketAjaxLink}
 import net.rrm.ehour.ui.common.wicket.AjaxLink.LinkCallback
@@ -14,9 +14,11 @@ import org.apache.wicket.util.convert.IConverter
 import org.apache.wicket.model.IModel
 import java.util.Date
 import net.rrm.ehour.ui.common.converter.DateConverter
+import org.apache.wicket.behavior.Behavior
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior
 
 
-class AjaxButton(id: String, form: Form[_], success: Callback, error: Callback = (a, f) => {}) extends WicketAjaxButton(id, form) {
+case class AjaxButton(id: String, form: Form[_], success: Callback, error: Callback = (a, f) => {}) extends WicketAjaxButton(id, form) {
   override def onSubmit(target: AjaxRequestTarget, form: Form[_]) {
     success(target, form)
   }
@@ -45,8 +47,8 @@ class Container(id: String) extends WebMarkupContainer(id) {
 }
 
 class AlwaysOnLabel[T <: java.io.Serializable](id: String, label: IModel[T], converter: Option[IConverter[T]] = None) extends Label(id, label) {
-  override def getConverter[T](`type`: Class[T]): IConverter[T] = converter match {
-    case Some(c) => c.asInstanceOf[IConverter[T]]
+  override def getConverter[X](`type`: Class[X]): IConverter[X] = converter match {
+    case Some(c) => c.asInstanceOf[IConverter[X]]
     case None => super.getConverter(`type`)
   }
 
@@ -55,4 +57,12 @@ class AlwaysOnLabel[T <: java.io.Serializable](id: String, label: IModel[T], con
 
 class DateLabel(id: String, model: IModel[Date]) extends AlwaysOnLabel(id, model, Some(new DateConverter))
 
+object WicketDSL {
+  def ajaxClick(click: AjaxRequestTarget => Unit): Behavior = new AjaxEventBehavior("onclick") {
+    def onEvent(target: AjaxRequestTarget) = click(target)
+  }
 
+  def ajaxSubmit(form: Form[Unit], submit: (Form[Unit], AjaxRequestTarget) => Unit): Behavior = new AjaxFormSubmitBehavior(form, "click") {
+    override def onSubmit(target: AjaxRequestTarget) = submit(form, target)
+  }
+}
