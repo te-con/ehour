63cdd1a139290569c6ad63429a9cd260bbb37719
Thies Edeling <thies@te-con.nl>
2013-12-04 01:29:36 +0100

    EHO-373 integration test added for PM managing project assignments

--- eHour-selenium/src/test/java/net/rrm/ehour/it/driver/ProjectManagementDriver.java
+++ eHour-selenium/src/test/java/net/rrm/ehour/it/driver/ProjectManagementDriver.java
@@ -4,8 +4,8 @@ import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
 import static net.rrm.ehour.it.AbstractScenario.BASE_URL;
 import static net.rrm.ehour.it.AbstractScenario.Driver;
 import static net.rrm.ehour.it.driver.CustomerManagementDriver.createActiveCustomer;
-import static net.rrm.ehour.it.driver.EhourApplicationDriver.loginAdmin;
-import static net.rrm.ehour.it.driver.EhourApplicationDriver.logout;
+import static net.rrm.ehour.it.driver.EhourApplicationDriver.*;
+import static net.rrm.ehour.it.driver.ItUtil.findElement;
 import static net.rrm.ehour.it.driver.ProjectDriver.*;
 import static net.rrm.ehour.it.driver.UserManagementDriver.createRegularUser;
 
@@ -32,5 +32,25 @@ public abstract class ProjectManagementDriver {
         logout();
 
         return user;
+    }
+
+    public static void clickFirstProject() {
+        findElement("entrySelectorFrame_entrySelectorFrame__body_projectSelector_entrySelectorFrame_blueBorder_blueBorder__body_itemListHolder_itemList_0").click();
+    }
+
+    public static void makeFirstAssignmentInActive() {
+        findElement("content_border_border__body_assignments_assignmentContainer_assignments_0_container").click();
+        sleep(500);
+        findElement("content_border_border__body_assignments_assignmentContainer_assignments_0_container_editForm_active").click();
+        findElement("content_border_border__body_assignments_assignmentContainer_assignments_0_container_editForm_submit").click();
+        sleep(500);
+    }
+
+    public static void submit() {
+        findElement("content_border_border__body_submitButton").click();
+    }
+
+    public static void assertAssignmentsUpdated() {
+        assertEquals("assignments updated", findElement("content_border_border__body_serverMessage").getText());
     }
 }
