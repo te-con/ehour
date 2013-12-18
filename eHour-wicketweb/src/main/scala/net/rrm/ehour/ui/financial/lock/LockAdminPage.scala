5df1b1c16e4ca7aa7acbfbeae040a7a242876e57
Thies Edeling <thies@te-con.nl>
2013-12-19 00:44:56 +0100

    EHO-62 styling

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockAdminPage.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockAdminPage.scala
@@ -4,15 +4,24 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import net.rrm.ehour.domain.UserRole
 import net.rrm.ehour.ui.common.page.AbstractBasePage
 import org.apache.wicket.model.ResourceModel
+import org.apache.wicket.request.resource.CssResourceReference
+import org.apache.wicket.markup.head.{CssHeaderItem, IHeaderResponse}
 
 @AuthorizeInstantiation(value = Array(UserRole.ROLE_ADMIN))
 class LockAdminPage extends AbstractBasePage[String](new ResourceModel("op.lock.admin.title")) {
+  val Css = new CssResourceReference(classOf[LockAdminPage], "timesheetlocks.css")
+
   override def onInitialize() {
     super.onInitialize()
 
     add(new ExistingLocksPanel(LockAdminPage.ExistingLocksId))
     add(new LockDetailsPanel(LockAdminPage.LockDetailsId))
   }
+
+  override def renderHead(response: IHeaderResponse) {
+    response.render(CssHeaderItem.forReference(Css))
+  }
+
 }
 
 object LockAdminPage {
