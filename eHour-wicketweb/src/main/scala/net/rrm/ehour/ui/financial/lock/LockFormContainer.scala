dff22a2a7031d6ae60d2d0bdfb163c0475f37a34
Thies Edeling <thies@te-con.nl>
2014-01-17 00:12:20 +0100

    EHO-62 creating lock form

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockFormContainer.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockFormContainer.scala
@@ -2,7 +2,13 @@ package net.rrm.ehour.ui.financial.lock
 
 import org.apache.wicket.model.Model
 import net.rrm.ehour.ui.common.panel.AbstractBasePanel
+import net.rrm.ehour.ui.common.wicket.Container
 
 class LockFormContainer (id: String, bean: LockAdminBackingBean) extends AbstractBasePanel(id, new Model[LockAdminBackingBean](bean)) {
+  override def onInitialize() {
+    super.onInitialize()
 
+    add(new LockFormPanel("lockFormPanel", getPanelModel))
+    add(new Container("affectedUsersPanel"))
+  }
 }
---
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/financial/lock/LockFormPanel.scala
@@ -0,0 +1,9 @@
+package net.rrm.ehour.ui.financial.lock
+
+import net.rrm.ehour.ui.common.panel.AbstractFormSubmittingPanel
+import org.apache.wicket.model.IModel
+
+class LockFormPanel(id: String, model: IModel[LockAdminBackingBean]) extends AbstractFormSubmittingPanel[LockAdminBackingBean](id, model) {
+
+
+}
