6bfaba9b02274977736572f63e9630a0dcb58ac7
Thies Edeling <thies@te-con.nl>
2014-04-13 00:03:21 +0200

    MX-3 named hibernate cache and properly closing hibernate session factory

--- eHour-persistence/src/test/groovy/net/rrm/ehour/persistence/appconfig/HibernateConfigurationTest.groovy
+++ eHour-persistence/src/test/groovy/net/rrm/ehour/persistence/appconfig/HibernateConfigurationTest.groovy
@@ -5,6 +5,7 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.springframework.test.annotation.DirtiesContext
 
 import javax.sql.DataSource
 import java.sql.Connection
@@ -17,44 +18,41 @@ import static org.mockito.Mockito.when
  * @author thies (Thies Edeling - thies@te-con.nl)
  * Created on: Nov 16, 2010 - 4:26:50 PM
  */
-class HibernateConfigurationTest
-{
-  @Mock
-  private DataSource dataSource
 
-  @Mock
-  private Connection connection
+class HibernateConfigurationTest {
+    @Mock
+    private DataSource dataSource
 
-  @Mock
-  private DatabaseMetaData metaData
+    @Mock
+    private Connection connection
 
-  @Before
-  public void setUp()
-  {
-    MockitoAnnotations.initMocks this
-    when(dataSource.connection).thenReturn connection
-    when(connection.metaData).thenReturn metaData
-  }
+    @Mock
+    private DatabaseMetaData metaData
 
-  @Test
-  void createSessionFactoryForDerby() throws Exception
-  {
-    assertNotNull(createSessionFactoryForDb("derby"));
-  }
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks this
+        when(dataSource.connection).thenReturn connection
+        when(connection.metaData).thenReturn metaData
+    }
 
-  @Test(expected = IllegalArgumentException)
-  void shouldNotCreateSessionFactoryForInvalidDatabase() throws Exception
-  {
-    assertNotNull(createSessionFactoryForDb("unknown"));
-    fail()
-  }
+    @Test
+    void createSessionFactoryForDerby() throws Exception {
+        def db = createSessionFactoryForDb("derby")
+        assertNotNull(db);
+        db.close()
 
-  private SessionFactory createSessionFactoryForDb(String db)
-  {
-    HibernateConfiguration configuration = new HibernateConfiguration(databaseName: db, dataSource: dataSource, caching: "true")
+    }
 
-    return configuration.getSessionFactory()
-  }
+    @Test(expected = IllegalArgumentException)
+    void shouldNotCreateSessionFactoryForInvalidDatabase() throws Exception {
+        assertNotNull(createSessionFactoryForDb("unknown"));
+        fail()
+    }
 
+    private SessionFactory createSessionFactoryForDb(String db) {
+        HibernateConfiguration configuration = new HibernateConfiguration(databaseName: db, dataSource: dataSource, caching: "true")
 
+        return configuration.getSessionFactory()
+    }
 }
