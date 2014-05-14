dfa7c8845107feef698df5c9c595c27d7365b75e
Thies Edeling <thies@te-con.nl>
2014-05-15 01:11:43 +0200

    MX-3 proper formatting of x-axis per aggregate by type

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/report/panel/detail/DetailedReportChartContainer.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/report/panel/detail/DetailedReportChartContainer.scala
@@ -2,19 +2,14 @@ package net.rrm.ehour.ui.report.panel.detail
 
 import org.apache.wicket.markup.html.panel.Panel
 import org.apache.wicket.markup.html.IHeaderContributor
-import org.apache.wicket.markup.head.{JavaScriptHeaderItem, OnLoadHeaderItem, IHeaderResponse}
-import org.apache.wicket.request.resource.JavaScriptResourceReference
+import org.apache.wicket.markup.head.{OnLoadHeaderItem, IHeaderResponse}
 import org.apache.wicket.event.IEvent
 import net.rrm.ehour.ui.report.panel.UpdateReportDataEvent
 
 class DetailedReportChartContainer(id: String, cacheKey: String) extends Panel(id) with IHeaderContributor {
   setOutputMarkupId(true)
 
-  private val JS = new JavaScriptResourceReference(classOf[DetailedReportChartContainer], "detailedReportChart.js")
-
   override def renderHead(response: IHeaderResponse) {
-    response.render(JavaScriptHeaderItem.forReference(JS))
-
     response.render(new OnLoadHeaderItem(s"window.chart = new DetailedReportChart('$cacheKey', 'chart');"))
 
     response.render(new OnLoadHeaderItem("window.chart.init();"))
