7c137deabf9df51e3bec61cde16831cbf49cf511
Thies Edeling <thies@te-con.nl>
2014-05-26 11:08:50 +0200

    MX-3 dates are now passed as strings rather than timestamps

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/common/chart/GsonSerializer.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/common/chart/GsonSerializer.scala
@@ -3,8 +3,11 @@ package net.rrm.ehour.ui.common.chart
 import com.google.gson._
 import org.joda.time.DateTime
 import java.lang.reflect.Type
+import org.joda.time.format.DateTimeFormat
 
 object GsonSerializer {
+  val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
+
   def create =
     new GsonBuilder()
       .registerTypeAdapter(classOf[DateTime], new DateTimeSerializer)
@@ -12,5 +15,5 @@ object GsonSerializer {
 }
 
 class DateTimeSerializer extends JsonSerializer[DateTime] {
-  override def serialize(d: DateTime, typeOfSrc: Type, context: JsonSerializationContext): JsonElement = new JsonPrimitive(d.getMillis)
+  override def serialize(d: DateTime, typeOfSrc: Type, context: JsonSerializationContext): JsonElement = new JsonPrimitive(GsonSerializer.formatter.print(d))
 }
