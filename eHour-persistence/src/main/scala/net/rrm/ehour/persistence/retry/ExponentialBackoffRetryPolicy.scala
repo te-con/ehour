4707967356fc4519c3eaf6f80fe3bd9199dec117
Thies Edeling <thies@te-con.nl>
2014-06-27 00:46:40 +0200

    spec added for retry policy

--- eHour-persistence/src/main/scala/net/rrm/ehour/persistence/retry/ExponentialBackoffRetryPolicy.scala
+++ eHour-persistence/src/main/scala/net/rrm/ehour/persistence/retry/ExponentialBackoffRetryPolicy.scala
@@ -7,35 +7,33 @@ import org.hibernate.HibernateException
 
 object ExponentialBackoffRetryPolicy {
   private final val MaxAttempts = 5
-  private final val LOG = Logger.getLogger(ExponentialBackoffRetryPolicy.getClass)
+  private final val Log = Logger.getLogger(ExponentialBackoffRetryPolicy.getClass)
 
-  def retry(op: () => AnyRef) = {
-    def execOrWait(waitTimes: Seq[Int], lastException: Option[HibernateException]): AnyRef = {
+  def retry[T](op: () => T, maxAttempts:Int = MaxAttempts) = {
+    def execOrWait(waitTimes: Seq[Int], lastException: Option[HibernateException]): T = {
       if (waitTimes.nonEmpty) {
         try {
           op()
         } catch {
           case e: HibernateException =>
             val waitTime = waitTimes.head
-            val attempt = MaxAttempts - waitTimes.length
-
-            LOG.warn(s"Attempt $attempt Failed to query, sleeping for $waitTime ms0", e)
+            val attempt = 1 + (maxAttempts - waitTimes.length)
+            Log.warn(s"Attempt $attempt: Failed to query, sleeping for $waitTime ms", e)
             Thread.sleep(waitTime)
             execOrWait(waitTimes.tail, Some(e))
         }
       } else {
         lastException match {
-          case Some(e) => {
-            LOG.warn("Giving up on query, max. attempts reached")
+          case Some(e) =>
+            Log.warn("Giving up on query, max. attempts reached")
             throw new RuntimeException(e)
-          }
           case None => throw new RuntimeException("No waittimes found")
         }
       }
     }
 
     val random = new Random()
-    val waitTimes = for (i <-  1 to MaxAttempts) yield { 1000 * Math.max(1, random.nextInt(1 << i)) }
+    val waitTimes = for (i <-  1 to maxAttempts) yield { 1000 * Math.max(1, random.nextInt(1 << i)) }
 
     execOrWait(waitTimes, None)
   }
--- eHour-persistence/src/test/scala/net/rrm/ehour/persistence/retry/ExponentialBackoffRetryPolicySpec.scala
+++ eHour-persistence/src/test/scala/net/rrm/ehour/persistence/retry/ExponentialBackoffRetryPolicySpec.scala
@@ -1,7 +1,41 @@
 package net.rrm.ehour.persistence.retry
 
 import net.rrm.ehour.AbstractSpec
+import org.hibernate.HibernateException
 
 class ExponentialBackoffRetryPolicySpec extends AbstractSpec {
+  "Exponential Backoff Retry Policy" should {
+    "execute with success" in {
+      def success() = true
 
+      ExponentialBackoffRetryPolicy.retry(success) should be (true)
+    }
+
+    "retry once" in {
+      var attempts = 0
+
+      def success() = {
+        if (attempts == 1) {
+          true
+        } else {
+          attempts = attempts + 1
+          throw new HibernateException("fail")
+        }
+      }
+
+      ExponentialBackoffRetryPolicy.retry(success) should be (true)
+      attempts should be (1)
+    }
+
+    "fail after 2 times" in {
+      def success() = throw new HibernateException("fail")
+
+      try {
+        ExponentialBackoffRetryPolicy.retry(success, 2)
+        fail()
+      } catch {
+        case e: RuntimeException =>
+      }
+    }
+  }
 }
