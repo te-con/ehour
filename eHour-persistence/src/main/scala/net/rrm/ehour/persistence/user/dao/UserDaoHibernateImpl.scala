f5264370883f9c0ffbd71811061f94b6286d7d5c
Thies Edeling <thies@te-con.nl>
2014-06-30 16:03:23 +0200

    failed to start server due to missing Hibernate session

--- eHour-persistence/src/main/scala/net/rrm/ehour/persistence/user/dao/UserDaoHibernateImpl.scala
+++ eHour-persistence/src/main/scala/net/rrm/ehour/persistence/user/dao/UserDaoHibernateImpl.scala
@@ -6,16 +6,20 @@ import net.rrm.ehour.domain.{User, UserDepartment}
 import net.rrm.ehour.persistence.dao.AbstractGenericDaoHibernateImpl
 import net.rrm.ehour.persistence.retry.ExponentialBackoffRetryPolicy
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
-@Repository("userDao") class UserDaoHibernateImpl extends AbstractGenericDaoHibernateImpl[Integer, User](classOf[User]) with UserDao {
+@Repository("userDao")
+class UserDaoHibernateImpl extends AbstractGenericDaoHibernateImpl[Integer, User](classOf[User]) with UserDao {
   private final val CacheRegion = Some("query.User")
 
+  @Transactional(readOnly = true)
   override def findByUsername(username: String): User = {
     val results = ExponentialBackoffRetryPolicy retry findByNamedQuery("User.findByUsername", "username", username, CacheRegion)
 
     if (results.size > 0) results.get(0) else null
   }
 
+  @Transactional(readOnly = true)
   override def findUsers(onlyActive: Boolean): util.List[User] = {
     if (onlyActive)
       findByNamedQuery("User.findActiveUsers", CacheRegion)
@@ -23,15 +27,19 @@ import org.springframework.stereotype.Repository
       findAll()
   }
 
+  @Transactional(readOnly = true)
   override def findActiveUsers(): util.List[User] = findUsers(onlyActive = true)
 
+  @Transactional(readOnly = true)
   override def findUsersForDepartments(departments: util.List[UserDepartment], onlyActive: Boolean): util.List[User] = {
     val hql = if (onlyActive) "User.findActiveForDepartment" else "User.findForDepartment"
     findByNamedQuery(hql, "departments", departments, CacheRegion)
   }
 
+  @Transactional(readOnly = true)
   override def findAllActiveUsersWithEmailSet(): util.List[User] = findByNamedQuery("User.findAllActiveUsersWithEmailSet", CacheRegion)
 
+  @Transactional
   override def deletePmWithoutProject() {
     ExponentialBackoffRetryPolicy retry getSession.getNamedQuery("User.deletePMsWithoutProject").executeUpdate
   }
