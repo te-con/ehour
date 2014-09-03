1611d10fb9ed14b6f97697990e81287956e14809
Thies Edeling <thies@te-con.nl>
2014-09-04 01:11:41 +0200

    white space

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/manage/customer/CustomerAdminBackingBean.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/manage/customer/CustomerAdminBackingBean.java
@@ -21,86 +21,72 @@ import net.rrm.ehour.ui.common.model.AdminBackingBeanImpl;
 
 /**
  * Backing bean for customer administration
- **/
+ */
 
-public class CustomerAdminBackingBean extends AdminBackingBeanImpl<Customer>
-{
-	private static final long serialVersionUID = 343538274642620123L;
-	private	Customer	customer;
-	
-	private String 	originalCustomerName;
-	private String	originalCustomerCode;
-	
-	public CustomerAdminBackingBean(Customer customer)
-	{
-		this.customer = customer;
-		
-		this.originalCustomerCode = customer.getCode();
-		this.originalCustomerName = customer.getName();
-	}
-	
-	/**
-	 * Create fresh new customer admin backing bean
-	 * @return
-	 */
-	public static CustomerAdminBackingBean createCustomerAdminBackingBean()
-	{
-		final Customer customer = new Customer();
-		customer.setActive(true);
-		
-		return new CustomerAdminBackingBean(customer);
-	}	
+public class CustomerAdminBackingBean extends AdminBackingBeanImpl<Customer> {
+    private static final long serialVersionUID = 343538274642620123L;
+    private Customer customer;
 
+    private String originalCustomerName;
+    private String originalCustomerCode;
 
-	public Customer getCustomer()
-	{
-		return customer;
-	}
+    public CustomerAdminBackingBean(Customer customer) {
+        this.customer = customer;
 
-	public void setCustomer(Customer customer)
-	{
-		this.customer = customer;
-	}
+        this.originalCustomerCode = customer.getCode();
+        this.originalCustomerName = customer.getName();
+    }
 
+    /**
+     * Create fresh new customer admin backing bean
+     *
+     * @return
+     */
+    public static CustomerAdminBackingBean createCustomerAdminBackingBean() {
+        final Customer customer = new Customer();
+        customer.setActive(true);
 
-	/**
-	 * @return the originalCustomerName
-	 */
-	public String getOriginalCustomerName()
-	{
-		return originalCustomerName;
-	}
+        return new CustomerAdminBackingBean(customer);
+    }
 
+    public Customer getCustomer() {
+        return customer;
+    }
 
-	/**
-	 * @param originalCustomerName the originalCustomerName to set
-	 */
-	public void setOriginalCustomerName(String originalCustomerName)
-	{
-		this.originalCustomerName = originalCustomerName;
-	}
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 
+    /**
+     * @return the originalCustomerName
+     */
+    public String getOriginalCustomerName() {
+        return originalCustomerName;
+    }
 
-	/**
-	 * @return the originalCustomerCode
-	 */
-	public String getOriginalCustomerCode()
-	{
-		return originalCustomerCode;
-	}
+    /**
+     * @param originalCustomerName the originalCustomerName to set
+     */
+    public void setOriginalCustomerName(String originalCustomerName) {
+        this.originalCustomerName = originalCustomerName;
+    }
 
+    /**
+     * @return the originalCustomerCode
+     */
+    public String getOriginalCustomerCode() {
+        return originalCustomerCode;
+    }
 
-	/**
-	 * @param originalCustomerCode the originalCustomerCode to set
-	 */
-	public void setOriginalCustomerCode(String originalCustomerCode)
-	{
-		this.originalCustomerCode = originalCustomerCode;
-	}
+    /**
+     * @param originalCustomerCode the originalCustomerCode to set
+     */
+    public void setOriginalCustomerCode(String originalCustomerCode) {
+        this.originalCustomerCode = originalCustomerCode;
+    }
 
     @Override
-	public Customer getDomainObject()
-	{
-		return getCustomer();
-	}
+    public Customer getDomainObject() {
+        return getCustomer();
+    }
 }
