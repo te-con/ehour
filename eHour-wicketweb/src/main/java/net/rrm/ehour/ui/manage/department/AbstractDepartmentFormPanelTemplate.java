eb4e4591b222eac050dc778695409ba04e93723c
Thies Edeling <thies@te-con.nl>
2014-09-04 01:26:42 +0200

    easier extensibility of the DepartmentFormPanel by moving common logic to a template class and providing a hook during form setup

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/manage/department/AbstractDepartmentFormPanelTemplate.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/manage/department/AbstractDepartmentFormPanelTemplate.java
@@ -1,26 +1,24 @@
 package net.rrm.ehour.ui.manage.department;
 
-import net.rrm.ehour.exception.ObjectNotUniqueException;
 import net.rrm.ehour.ui.common.border.GreySquaredRoundedBorder;
 import net.rrm.ehour.ui.common.component.AjaxFormComponentFeedbackIndicator;
 import net.rrm.ehour.ui.common.component.ServerMessageLabel;
 import net.rrm.ehour.ui.common.component.ValidatingFormComponentAjaxBehavior;
-import net.rrm.ehour.ui.common.event.AjaxEventType;
 import net.rrm.ehour.ui.common.form.FormConfig;
 import net.rrm.ehour.ui.common.form.FormUtil;
 import net.rrm.ehour.ui.common.model.AdminBackingBean;
 import net.rrm.ehour.ui.common.panel.AbstractFormSubmittingPanel;
 import net.rrm.ehour.ui.common.util.WebGeo;
 import net.rrm.ehour.user.service.UserService;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.validator.StringValidator;
 
-public class AbstractDepartmentFormPanelTemplate<T extends AdminBackingBean> extends AbstractFormSubmittingPanel<T> {
+public abstract class AbstractDepartmentFormPanelTemplate<T extends AdminBackingBean> extends AbstractFormSubmittingPanel<T> {
     private static final long serialVersionUID = -6469066920645156569L;
 
     @SpringBean
@@ -28,12 +26,18 @@ public class AbstractDepartmentFormPanelTemplate<T extends AdminBackingBean> ext
 
     public AbstractDepartmentFormPanelTemplate(String id, CompoundPropertyModel<T> model) {
         super(id, model);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
 
         GreySquaredRoundedBorder greyBorder = new GreySquaredRoundedBorder("border", WebGeo.AUTO);
         add(greyBorder);
 
         setOutputMarkupId(true);
 
+        IModel<T> model = getPanelModel();
         final Form<T> form = new Form<T>("deptForm", model);
 
         // name
@@ -62,25 +66,11 @@ public class AbstractDepartmentFormPanelTemplate<T extends AdminBackingBean> ext
         FormUtil.setSubmitActions(formConfig);
 
         greyBorder.add(form);
+
+        onFormCreated(form);
     }
 
-    private void persistDepartment(DepartmentAdminBackingBean backingBean) throws ObjectNotUniqueException {
-        userService.persistUserDepartment(backingBean.getDepartment());
-    }
+    protected void onFormCreated(Form<T> form) {
 
-    private void deleteDepartment(DepartmentAdminBackingBean backingBean) {
-        userService.deleteDepartment(backingBean.getDepartment().getDepartmentId());
-    }
-
-    @Override
-    protected boolean processFormSubmit(AjaxRequestTarget target, AdminBackingBean backingBean, AjaxEventType type) throws Exception {
-        DepartmentAdminBackingBean departmentBackingBean = (DepartmentAdminBackingBean) backingBean;
-
-        if (type == DepartmentAjaxEventType.DEPARTMENT_UPDATED) {
-            persistDepartment(departmentBackingBean);
-        } else if (type == DepartmentAjaxEventType.DEPARTMENT_DELETED) {
-            deleteDepartment(departmentBackingBean);
-        }
-        return true;
     }
 }
