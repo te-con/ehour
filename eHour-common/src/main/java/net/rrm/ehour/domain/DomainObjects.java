88eb5b140186f29419dc3af7e1e3267e33a77dc7
Thies Edeling <thies@te-con.nl>
2014-09-09 15:12:20 +0200

    EHV-29: Created update script for MySQL. Made some minor modifications in DDL and DML for derby as well to be consistent with other tables.
    Conflicts:
    	eHour-common/src/main/java/net/rrm/ehour/domain/DomainObjects.java
    	eHour-service/src/main/java/net/rrm/ehour/timesheet/service/TimesheetService.java
    	eHour-standalone/src/test/resources/conf/ehour.properties

--- eHour-common/src/main/java/net/rrm/ehour/domain/DomainObjects.java
+++ eHour-common/src/main/java/net/rrm/ehour/domain/DomainObjects.java
@@ -12,7 +12,6 @@ public class DomainObjects
                                                             MailLog.class,
                                                             TimesheetEntry.class, TimesheetComment.class,
                                                             TimesheetLock.class,
-                                                            Activity.class,
-                                                            ApprovalStatus.class
+                                                            Activity.class
     };
 }
