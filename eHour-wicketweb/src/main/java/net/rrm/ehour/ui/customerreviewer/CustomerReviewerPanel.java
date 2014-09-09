a0c6bf2369202e00e6866a6a24d27f3e73d6f22c
Thies Edeling <thies@te-con.nl>
2014-09-09 15:01:53 +0200

    EHV-4: Implemented the Link for View Timesheet, Accept and Reject Links in Customer Reviewer Page.

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/customerreviewer/CustomerReviewerPanel.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/customerreviewer/CustomerReviewerPanel.java
@@ -23,12 +23,12 @@ import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulato
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.border.Border;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 import org.apache.wicket.markup.html.resources.StyleSheetReference;
 import org.apache.wicket.markup.repeater.Item;
@@ -67,45 +67,54 @@ public class CustomerReviewerPanel extends AbstractAjaxPanel<ReportCriteria> {
 		Border greyBorder = new GreyBlueRoundedBorder("border");
 		add(greyBorder);
 
+		final Form<Void> form = new Form<Void>("customerReviewerForm");
+
 		@SuppressWarnings("unchecked")
-		IColumn<UserProjectStatus>[] columns = new IColumn[4];
-		columns[0] = new PropertyColumn<UserProjectStatus>(new ResourceModel("customerreviewer.timesheet.column.customer"), "activity.project.customer.name");
-		columns[1] = new PropertyColumn<UserProjectStatus>(new ResourceModel("customerreviewer.timesheet.column.user"), "activity.assignedUser.firstName");
+		IColumn<UserProjectStatus>[] columns = new IColumn[6];
+		columns[0] = new PropertyColumn<UserProjectStatus>(new ResourceModel("customerreviewer.timesheet.column.customer"),
+				"activity.project.customer.name");
+		columns[1] = new PropertyColumn<UserProjectStatus>(new ResourceModel("customerreviewer.timesheet.column.user"),
+				"activity.assignedUser.firstName");
 		columns[2] = new DateColumn(new ResourceModel("customerreviewer.timesheet.column.period"), getConfig());
-		columns[3] = new AbstractColumn<UserProjectStatus>(new Model<String>("timesheet")) {
+		columns[3] = new AbstractColumn<UserProjectStatus>(new Model<String>("view")) {
 			@SuppressWarnings({ "rawtypes", "unchecked" })
 			@Override
 			public void populateItem(final Item item, String compId, final IModel model) {
-				final UserProjectStatus userProjectStatus = (UserProjectStatus) model.getObject();
-				item.add(new Link(compId, new Model("timesheet")) {
-					@Override
-					public void onClick() {
-						UserOverviewPage userOverviewPage = new UserOverviewPage(userProjectStatus.getActivity().getAssignedUser());
-						setResponsePage(userOverviewPage);
-					}
-					
-					protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
-						replaceComponentTagBody(markupStream, openTag, "view");
-					};
-					
-				});
+				item.add(new ViewTimesheetPanel(compId, model));
 			}
 		};
-		
-		AjaxFallbackDefaultDataTable<UserProjectStatus> table = new AjaxFallbackDefaultDataTable<UserProjectStatus>("data", columns, new CustomerReviewerDataProvider(statusses), 20);
+		columns[4] = new AbstractColumn<UserProjectStatus>(new Model<String>("approve")) {
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			@Override
+			public void populateItem(final Item item, String compId, final IModel model) {
+				item.add(new AcceptPanel(compId, model));
+			}
+		};
+
+		columns[5] = new AbstractColumn<UserProjectStatus>(new Model<String>("reject")) {
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			@Override
+			public void populateItem(final Item item, String compId, final IModel model) {
+				item.add(new RejectPanel(compId, model));
+			}
+		};
+
+		AjaxFallbackDefaultDataTable<UserProjectStatus> table = new AjaxFallbackDefaultDataTable<UserProjectStatus>("data", columns,
+				new CustomerReviewerDataProvider(statusses), 20);
+
+		form.setOutputMarkupId(true);
+		form.add(table);
+
 		final WebMarkupContainer dataContainer = new WebMarkupContainer("dataContainer");
 		dataContainer.setOutputMarkupId(true);
-		dataContainer.add(table);
+		dataContainer.add(form);
 		greyBorder.add(dataContainer);
 	}
-	
-	private class DateColumn extends AbstractColumn<UserProjectStatus>
-	{
-		private static final long serialVersionUID = -5517077439980001335L;
+
+	private class DateColumn extends AbstractColumn<UserProjectStatus> {
 		private EhourConfig config;
-		
-		public DateColumn(IModel<String> displayModel, EhourConfig config)
-		{
+
+		public DateColumn(IModel<String> displayModel, EhourConfig config) {
 			super(displayModel);
 			this.config = config;
 		}
@@ -119,4 +128,45 @@ public class CustomerReviewerPanel extends AbstractAjaxPanel<ReportCriteria> {
 		}
 
 	}
+
+	class RejectPanel extends Panel {
+		
+		public RejectPanel(String id, IModel<UserProjectStatus> model) {
+			super(id, model);
+			add(new Link("reject") {
+				@Override
+				public void onClick() {
+				}
+			});
+		}
+	}
+	
+	class AcceptPanel extends Panel {
+		
+		public AcceptPanel(String id, IModel<UserProjectStatus> model) {
+			super(id, model);
+			add(new Link("accept") {
+				@Override
+				public void onClick() {
+				}
+			});
+		}
+	}
+	
+	class ViewTimesheetPanel extends Panel {
+
+		public ViewTimesheetPanel(String id, IModel<UserProjectStatus> model) {
+			super(id, model);
+			final UserProjectStatus userProjectStatus = (UserProjectStatus) model.getObject();
+			add(new Link("view") {
+				@Override
+				public void onClick() {
+					 UserOverviewPage userOverviewPage = new
+					 UserOverviewPage(userProjectStatus.getActivity().getAssignedUser());
+					 setResponsePage(userOverviewPage);
+				}
+			});
+		}
+	}
+	
 }
