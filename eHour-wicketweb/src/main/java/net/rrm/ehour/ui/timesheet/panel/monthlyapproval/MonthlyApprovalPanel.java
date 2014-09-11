051dc062945ea37d677a8ab9353ee0c71a166a65
Thies Edeling <thies@te-con.nl>
2014-09-11 13:26:34 +0200

    EHV1: Work done for Ajax refresh of the approval status for the user

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/timesheet/panel/monthlyapproval/MonthlyApprovalPanel.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/timesheet/panel/monthlyapproval/MonthlyApprovalPanel.java
@@ -33,7 +33,10 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
  **/
 
 public class MonthlyApprovalPanel extends Panel {
+
 	private static final long serialVersionUID = 7854272569953832030L;
+	
+	private CustomTitledGreyRoundedBorder greyBorder;
 
 	@SpringBean
 	private ApprovalStatusService approvalStatusService;
@@ -54,8 +57,8 @@ public class MonthlyApprovalPanel extends Panel {
 
 	private void addPanelComponents(Calendar overviewFor, SortedSet<UserProjectStatus> userProjectStatuses) {
 
-		CustomTitledGreyRoundedBorder greyBorder = new CustomTitledGreyRoundedBorder("greyBorder", new Label("title", new ResourceModel(
-				"monthlyapproval.title")), WebGeo.W_CONTENT_MEDIUM);
+		greyBorder = new CustomTitledGreyRoundedBorder("greyBorder", new Label("title", new ResourceModel("monthlyapproval.title")), WebGeo.W_CONTENT_MEDIUM);
+		greyBorder.setOutputMarkupId(true);
 
 		List<Customer> customers = getAllCustomersForWhichThisUserWorks(userProjectStatuses);
 
@@ -69,9 +72,10 @@ public class MonthlyApprovalPanel extends Panel {
 				Label customerLabel = new Label("customerName", customer.getName());
 				item.add(customerLabel);
 
-				Label appprovalStatusLabel = new Label("approvalStatus", approvalStatusService
+				final Label approvalStatusLabel = new Label("approvalStatus", approvalStatusService
 						.getApprovalStatusForUserWorkingForCustomer(user, customer, monthRange).get(0).getStatus().toString());
-				item.add(appprovalStatusLabel);
+				item.add(approvalStatusLabel);
+				approvalStatusLabel.setOutputMarkupId(true);
 
 				Form monthlyApprovalStatusForm = new Form("monthlyApprovalStatusForm", item.getDefaultModel());
 
@@ -81,14 +85,14 @@ public class MonthlyApprovalPanel extends Panel {
 					@Override
 					public void onClick(AjaxRequestTarget target) {
 						target.addComponent(this);
+						target.addComponent(greyBorder);
 						Customer customer = item.getModelObject();
 						changeApprovalStatus(customer);
-						setVisibilityOfApprovalLink(customer, this);
+						setVisibilityOfApprovalLinkAndUpdateStatus(customer, this, approvalStatusLabel);
 					}
 
 					private void changeApprovalStatus(Customer customer) {
-						List<ApprovalStatus> currentApprovalStatus = approvalStatusService.getApprovalStatusForUserWorkingForCustomer(user,
-								customer, monthRange);
+						List<ApprovalStatus> currentApprovalStatus = approvalStatusService.getApprovalStatusForUserWorkingForCustomer(user, customer, monthRange);
 						// TODO Can be handled in better way.
 						// Should get only element in the list
 						ApprovalStatus approvalStatus = currentApprovalStatus.get(0);
@@ -97,7 +101,7 @@ public class MonthlyApprovalPanel extends Panel {
 					}
 				};
 
-				setVisibilityOfApprovalLink(item.getModelObject(), requestApprovalLink);
+				setVisibilityOfApprovalLinkAndUpdateStatus(item.getModelObject(), requestApprovalLink, approvalStatusLabel);
 
 				monthlyApprovalStatusForm.add(requestApprovalLink);
 				item.add(monthlyApprovalStatusForm);
@@ -108,10 +112,13 @@ public class MonthlyApprovalPanel extends Panel {
 		add(greyBorder);
 	}
 
-	private void setVisibilityOfApprovalLink(Customer customer, AjaxLink<Void> requestApprovalLink) {
-		List<ApprovalStatus> approvalStatusForActivity = approvalStatusService.getApprovalStatusForUserWorkingForCustomer(this.user,
-				customer, this.monthRange);
-		ApprovalStatusType approvalStatusType = approvalStatusForActivity.iterator().next().getStatus();
+	private void setVisibilityOfApprovalLinkAndUpdateStatus(Customer customer, AjaxLink<Void> requestApprovalLink, Label appprovalStatusLabel) {
+		List<ApprovalStatus> approvalStatusForCustomer = approvalStatusService.getApprovalStatusForUserWorkingForCustomer(this.user, customer, this.monthRange);
+		ApprovalStatusType approvalStatusType = approvalStatusForCustomer.iterator().next().getStatus();
+		
+		if(approvalStatusType != null) {
+			appprovalStatusLabel = new Label("approvalStatus", approvalStatusType.toString());
+		}
 
 		if (approvalStatusType.equals(ApprovalStatusType.IN_PROGRESS) || approvalStatusType.equals(ApprovalStatusType.REJECTED)) {
 			requestApprovalLink.setVisible(true);
