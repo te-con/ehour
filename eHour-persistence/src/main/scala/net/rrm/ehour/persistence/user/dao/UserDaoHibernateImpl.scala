1428af4ec836d7821f9b051678b669a2774ae088
Thies Edeling <thies@te-con.nl>
2014-09-15 12:43:04 +0200

    user pref dao convert

--- eHour-persistence/src/main/scala/net/rrm/ehour/persistence/user/dao/UserDaoHibernateImpl.scala
+++ eHour-persistence/src/main/scala/net/rrm/ehour/persistence/user/dao/UserDaoHibernateImpl.scala
@@ -2,8 +2,7 @@ package net.rrm.ehour.persistence.user.dao
 
 import java.util
 
-import net.rrm.ehour.domain.UserRole.CUSTOMERREPORTER
-import net.rrm.ehour.domain.{UserRole, User, UserDepartment}
+import net.rrm.ehour.domain.{User, UserDepartment, UserRole}
 import net.rrm.ehour.persistence.dao.AbstractGenericDaoHibernateImpl
 import net.rrm.ehour.persistence.retry.ExponentialBackoffRetryPolicy
 import org.springframework.stereotype.Repository
@@ -47,15 +46,18 @@ class UserDaoHibernateImpl extends AbstractGenericDaoHibernateImpl[Integer, User
 
   @Transactional
   override def cleanRedundantRoleInformation(userRole: UserRole): Unit = {
+    val x = UserRole.CUSTOMERREPORTER
+    val y = UserRole.CUSTOMERREVIEWER
+
     val q = userRole match {
-      case _: UserRole.CUSTOMERREVIEWER => Some("User.deleteRedundantReviewers")
-      case role: UserRole.CUSTOMERREPORTER => Some("User.deleteRedundantReviewers")
+      case `x` => Some("User.deleteRedundantReviewers")
+      case `y` => Some("User.deleteRedundantReporters")
       case _ => None
     }
 
     q match {
       case Some(query) => ExponentialBackoffRetryPolicy retry getSession.getNamedQuery(query).executeUpdate
-      case None => _
+      case None =>
     }
   }
 }
---
+++ eHour-persistence/src/main/scala/net/rrm/ehour/persistence/userpref/dao/UserPreferencesDao.scala
@@ -0,0 +1,19 @@
+package net.rrm.ehour.persistence.userpref.dao
+
+import net.rrm.ehour.domain._
+import net.rrm.ehour.persistence.dao.{AbstractGenericDaoHibernateImpl, GenericDao}
+
+trait UserPreferencesDao extends GenericDao[UserPreferenceId, UserPreference] {
+  def getUserPreferenceForUserForType(user: User, userPreferenceType: UserPreferenceType): UserPreference
+}
+
+class UserPreferencesDaoHibernateImpl extends AbstractGenericDaoHibernateImpl[UserPreferenceId, UserPreference](classOf[UserPreference]) with UserPreferencesDao {
+  override def getUserPreferenceForUserForType(user: User, userPreferenceType: UserPreferenceType): UserPreference = {
+    val keys = List("userPreferenceKey", "user")
+    val params = List(userPreferenceType.getValue, user)
+
+    val results = findByNamedQuery("UserPreference.getUserPreferenceForUserForType", keys, params)
+
+    if (results.isEmpty) null else results.get(0)
+  }
+}
