62ff3087e5b9a310c9c04971bb8877b6f22e48db
Thies Edeling <thies@te-con.nl>
2014-09-23 13:44:41 +0200

    convenience method for user department with a different id added

--- eHour-common/src/test/java/net/rrm/ehour/domain/UserDepartmentObjectMother.java
+++ eHour-common/src/test/java/net/rrm/ehour/domain/UserDepartmentObjectMother.java
@@ -6,18 +6,20 @@ import com.google.common.collect.Sets;
  * Created on Feb 7, 2010 1:50:16 PM
  *
  * @author thies (www.te-con.nl)
- *
  */
-public class UserDepartmentObjectMother
-{
-	public static UserDepartment createUserDepartment()
-	{
-		UserDepartment department = new UserDepartment(1);
-		department.setCode("aa");
-		department.setName("bb");
-		
-		department.setUsers(Sets.newHashSet(UserObjectMother.createUsers(department)));
-		return department;
-	}
+public class UserDepartmentObjectMother {
+    public static UserDepartment createUserDepartment() {
+        return createUserDepartment(1);
+    }
+
+    public static UserDepartment createUserDepartment(int departmentId) {
+        UserDepartment department = new UserDepartment(departmentId);
+        department.setCode("aa");
+        department.setName("bb");
+
+        department.setUsers(Sets.newHashSet(UserObjectMother.createUsers(department)));
+        return department;
+    }
+
 
 }
