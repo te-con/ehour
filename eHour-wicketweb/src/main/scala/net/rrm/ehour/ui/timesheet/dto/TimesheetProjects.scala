a0b784478da22b747d6e08da72c142ef5614de49
Thies Edeling <thies@te-con.nl>
2014-10-01 00:14:55 +0200

    the lazy activity collection of activities is replaced with a concrete collection of activities to prevent roundtrips to the db
    Conflicts:
    	eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/timesheet/dto/TimesheetProjects.scala

--- eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/timesheet/dto/TimesheetProjects.scala
+++ eHour-wicketweb/src/main/scala/net/rrm/ehour/ui/timesheet/dto/TimesheetProjects.scala
@@ -2,49 +2,52 @@ package net.rrm.ehour.ui.timesheet.dto
 
 import java.lang.Iterable
 import java.util
+import java.util.{ArrayList => JavaArrayList, List => JavaList}
 
-import net.rrm.ehour.domain.Project
-
+import net.rrm.ehour.domain.{Activity, Project}
 import scala.collection.JavaConversions._
-import scala.collection.convert.WrapAsJava
-import scala.collection.immutable
-import scala.collection.mutable.{ListBuffer, Map => MutableMap}
+import scala.collection.immutable.TreeMap
 
 protected object TimesheetProjects {
-  def apply(rows: util.List[TimesheetRow]): TimesheetProjects = {
+  def apply(rows: JavaList[TimesheetRow]): TimesheetProjects = {
 
-    val projects = MutableMap[Project, ListBuffer[TimesheetRow]]()
+    val xs: Seq[TimesheetRow] = rows
 
-    rows.foreach(f => {
-      val project = f.getActivity.getProject
-      val projectRows = projects.getOrElse(project, new ListBuffer[TimesheetRow]())
-      projectRows += f
-      projects.put(project, projectRows)
-    })
+    val activities = xs.map(_.getActivity)
+    val projectActivityMap: Map[Project, Seq[Activity]] = activities.groupBy(_.getProject)
 
-    val projectMap = for (prj <- projects.toArray) yield (prj._1, prj._2.toList)
+    for (project <- projectActivityMap) yield {
+      val activities: JavaList[Activity] = project._2
+      project._1.setActivities(new util.HashSet(activities))
+    }
 
-    def compare(a: (Project, _), b: (Project, _)) = a._1.getName < b._1.getName
+    val projects = xs.groupBy(_.getActivity.getProject)
 
-    new TimesheetProjects(projectMap.toSeq.sortWith(compare).toMap)
+    val projectMap: Array[(Project, Seq[TimesheetRow])] = for (prj <- projects.toArray) yield (prj._1, prj._2)
+
+    val projectOrder = new Ordering[Project] {
+      def compare(a: Project, b: Project) = a.getName.compareToIgnoreCase(b.getName)
+    }
+
+    new TimesheetProjects(TreeMap(projectMap: _*)(projectOrder))
   }
 }
 
-class TimesheetProjects(projectMap: Map[Project, immutable.List[TimesheetRow]]) extends Serializable {
+class TimesheetProjects(projectMap: Map[Project, JavaList[TimesheetRow]]) extends Serializable {
   type Predicate[T] = (T => Boolean)
 
-  def get(): util.List[Project] = {
-    new util.ArrayList(projectMap.keySet
+  def get(): JavaList[Project] = {
+    new JavaArrayList(projectMap.keySet
       .toList
       .sortWith((a: Project, b: Project) => a.getName.compareToIgnoreCase(b.getName) < 0)
       )
   }
 
-  def getTimesheetRow(project: Project): util.List[TimesheetRow] = {
-    val rows = projectMap.getOrElse(project, List())
+  def getTimesheetRow(project: Project): JavaList[TimesheetRow] = {
 
+    val rows = projectMap.getOrElse(project, new JavaArrayList[TimesheetRow])
     rows.sortWith((a: TimesheetRow, b: TimesheetRow) => a.getActivity.getName.compareToIgnoreCase(b.getActivity.getName) < 0)
   }
 
-  def getTimesheetRows: Iterable[util.List[TimesheetRow]] = projectMap.toSeq.map(p => WrapAsJava.seqAsJavaList(p._2))
+  def getTimesheetRows: Iterable[JavaList[TimesheetRow]] = projectMap.values
 }
