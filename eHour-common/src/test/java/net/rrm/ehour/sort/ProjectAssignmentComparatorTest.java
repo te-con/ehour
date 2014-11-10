371ae82538e3e2350fe2e96cdb595eb2a4edec36
Thies Edeling <thies@te-con.nl>
2014-11-10 15:25:51 +0100

    weird package fixes

--- eHour-common/src/test/java/net/rrm/ehour/sort/ProjectAssignmentComparatorTest.java
+++ eHour-common/src/test/java/net/rrm/ehour/sort/ProjectAssignmentComparatorTest.java
@@ -14,10 +14,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-package net.rrm.ehour.ui.common.sort;
+package net.rrm.ehour.sort;
 
 import net.rrm.ehour.domain.ProjectAssignmentObjectMother;
-import net.rrm.ehour.sort.ProjectAssignmentComparator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
