7866d138df3fe3ec1646fb2a413e8248f0ede4c9
Thies Edeling <thies@te-con.nl>
2014-12-11 02:01:13 +0100

    passing model rather than object

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/timesheet/panel/renderer/TimesheetIconRenderFactory.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/timesheet/panel/renderer/TimesheetIconRenderFactory.java
@@ -4,7 +4,8 @@ import net.rrm.ehour.ui.timesheet.dto.TimesheetCell;
 import net.rrm.ehour.ui.timesheet.model.TimesheetContainer;
 import net.rrm.ehour.ui.timesheet.panel.TimesheetRowList;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 
 public interface TimesheetIconRenderFactory extends PrioritizedRenderFactory {
-    Panel renderForId(String id, TimesheetCell cell, TimesheetRowList.DayStatus status, TimesheetContainer timesheetContainer);
+    Panel renderForId(String id, TimesheetCell cell, TimesheetRowList.DayStatus status, IModel<TimesheetContainer> timesheetContainerModel);
 }
