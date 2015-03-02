0a9672d2366904fdc311064c35d5d25b15921962
Thies Edeling <thies@te-con.nl>
2015-03-02 14:42:13 +0100

    dropped support for c3p0

--- eHour-persistence/src/main/java/net/rrm/ehour/persistence/datasource/SupportedDatabases.java
+++ eHour-persistence/src/main/java/net/rrm/ehour/persistence/datasource/SupportedDatabases.java
@@ -1,6 +1,5 @@
 package net.rrm.ehour.persistence.datasource;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.lang.StringUtils;
@@ -26,21 +25,13 @@ public enum SupportedDatabases {
     MYSQL {
         @Override
         public DataSource createDatasource(DatasourceConfiguration config) throws IOException, PropertyVetoException, URISyntaxException {
-            if (DatasourceConfiguration.CP_HIKARI.equalsIgnoreCase(config.connectionPool)) {
-                return createHikariDatasource(config, "mysql");
-            } else {
-                return createC3p0Datasource(config, "mysql", "com.mysql.jdbc.Driver");
-            }
+            return createHikariDatasource(config, "mysql");
         }
     },
     POSTGRESQL {
         @Override
         public DataSource createDatasource(DatasourceConfiguration config) throws IOException, PropertyVetoException, URISyntaxException {
-            if (DatasourceConfiguration.CP_HIKARI.equalsIgnoreCase(config.connectionPool)) {
-                return createHikariDatasource(config, "postgresql");
-            } else {
-                return createC3p0Datasource(config, "postgresql", "org.postgresql.Driver");
-            }
+            return createHikariDatasource(config, "postgresql");
         }
     };
 
@@ -89,64 +80,6 @@ public enum SupportedDatabases {
         hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
 
         return new HikariDataSource(hikariConfig);
-    }
-
-    private static DataSource createC3p0Datasource(DatasourceConfiguration config, String db, String defaultDriverClass) throws IOException, PropertyVetoException, URISyntaxException {
-        ComboPooledDataSource ds = new ComboPooledDataSource();
-
-        String username = config.username;
-        String password = config.password;
-
-        // try to extract username/password from the db URL when they're not provided
-        if (StringUtils.isBlank(config.username) && StringUtils.isBlank(password)) {
-            URI dbUri = new URI(config.url);
-
-            String userInfo = dbUri.getUserInfo();
-
-            if (StringUtils.isNotBlank(userInfo)) {
-                String[] splitted = userInfo.split(":");
-                username = splitted[0];
-                password = splitted[1];
-            }
-
-            String dbUrl = String.format("jdbc:%s://%s:%d%s", db, dbUri.getHost(), dbUri.getPort(), dbUri.getPath());
-
-            LOGGER.info("Only a DB URL was provided, stripped of username and password and connecting to " + dbUrl);
-
-            verify(username, password);
-            verify("ehour.database.url", dbUrl);
-
-            ds.setJdbcUrl(dbUrl);
-        } else {
-            verify(username, password);
-            verify("ehour.database.url", config.url);
-            ds.setJdbcUrl(config.url);
-        }
-
-        ds.setUser(username);
-        ds.setPassword(password);
-
-        ds.setNumHelperThreads(10);
-
-        String driverClass = StringUtils.isNotBlank(config.driver) ? config.driver : defaultDriverClass;
-        verify("ehour.database.driver", driverClass);
-        LOGGER.info("Using driver class: " + driverClass);
-
-        ds.setDriverClass(driverClass);
-        ds.setInitialPoolSize(10);
-        ds.setAcquireIncrement(2);
-
-        ds.setIdleConnectionTestPeriod(30);
-        ds.setTestConnectionOnCheckout(false);
-        ds.setTestConnectionOnCheckin(true);
-
-        ds.setMaxPoolSize(100);
-        ds.setMaxStatements(0);
-        ds.setMinPoolSize(10);
-        ds.setCheckoutTimeout(config.checkoutTimeout);
-
-        LOGGER.info(config.toString());
-        return ds;
     }
 
     private static void verify(String username, String password) {
