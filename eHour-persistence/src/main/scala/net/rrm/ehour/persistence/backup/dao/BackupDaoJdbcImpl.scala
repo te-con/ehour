a32927f6bfb5a357f3c024262d97680e756ab35c
Thies Edeling <thies@te-con.nl>
2015-03-02 15:53:37 +0100

    replaced jdbc template with hibernate native sql, one dependency less

--- eHour-persistence/src/main/scala/net/rrm/ehour/persistence/backup/dao/BackupDaoJdbcImpl.scala
+++ eHour-persistence/src/main/scala/net/rrm/ehour/persistence/backup/dao/BackupDaoJdbcImpl.scala
@@ -2,16 +2,18 @@ package net.rrm.ehour.persistence.backup.dao
 
 import java.util
 
-import net.rrm.ehour.persistence.retry.ExponentialBackoffRetryPolicy
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.jdbc.core.JdbcTemplate
+import net.rrm.ehour.persistence.dao.AbstractAnnotationDaoHibernate4Impl
+import org.hibernate.transform.Transformers
 import org.springframework.stereotype.Repository
 
 @Repository("exportDao")
-class BackupDaoJdbcImpl extends BackupDao {
-  @Autowired
-  var jdbcTemplate: JdbcTemplate = _
+class BackupDaoJdbcImpl extends AbstractAnnotationDaoHibernate4Impl with BackupDao {
+  override def findAll(table: String): util.List[util.Map[String, Object]] = {
+    getSession
+        .createSQLQuery("SELECT * FROM " + table)
+        .setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP)
+        .list()
+        .asInstanceOf[util.List[util.Map[String, Object]]]
+  }
 
-  override def findAll(table: String): util.List[util.Map[String, Object]] =
-    ExponentialBackoffRetryPolicy retry jdbcTemplate.queryForList("SELECT * FROM " + table)
 }
