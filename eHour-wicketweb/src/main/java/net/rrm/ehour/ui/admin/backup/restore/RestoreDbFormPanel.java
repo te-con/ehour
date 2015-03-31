cf639e100c55f75a9808eaef3e0b3d9eaf6e5973
Thies Edeling <thies@te-con.nl>
2015-03-31 13:48:27 +0200

    i18n button keys for restore panel added

--- eHour-wicketweb/src/main/java/net/rrm/ehour/ui/admin/backup/restore/RestoreDbFormPanel.java
+++ eHour-wicketweb/src/main/java/net/rrm/ehour/ui/admin/backup/restore/RestoreDbFormPanel.java
@@ -7,13 +7,19 @@ import net.rrm.ehour.backup.service.restore.RestoreService;
 import net.rrm.ehour.ui.admin.backup.BackupAjaxEventType;
 import net.rrm.ehour.ui.common.border.GreyBlueRoundedBorder;
 import net.rrm.ehour.ui.common.border.GreyRoundedBorder;
+import net.rrm.ehour.ui.common.component.JavaScriptConfirmation;
+import net.rrm.ehour.ui.common.decorator.DemoDecorator;
+import net.rrm.ehour.ui.common.decorator.LoadingSpinnerDecorator;
 import net.rrm.ehour.ui.common.event.AjaxEvent;
 import net.rrm.ehour.ui.common.event.PayloadAjaxEvent;
 import net.rrm.ehour.ui.common.panel.AbstractAjaxPanel;
+import net.rrm.ehour.ui.common.session.EhourWebSession;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
+import org.apache.wicket.ajax.attributes.IAjaxCallListener;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxLazyLoadPanel;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
@@ -25,6 +31,7 @@ import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.time.Duration;
 import org.wicketstuff.async.components.IRunnableFactory;
+import org.wicketstuff.async.components.InteractionState;
 import org.wicketstuff.async.components.ProgressBar;
 import org.wicketstuff.async.components.ProgressButton;
 import org.wicketstuff.async.task.AbstractTaskContainer;
@@ -32,6 +39,7 @@ import org.wicketstuff.async.task.DefaultTaskManager;
 import org.wicketstuff.async.task.IProgressObservableRunnable;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class RestoreDbFormPanel extends AbstractAjaxPanel<Void> {
@@ -69,6 +77,8 @@ public class RestoreDbFormPanel extends AbstractAjaxPanel<Void> {
     }
 
     private Form<Void> addUploadForm(String id) {
+        final boolean inDemoMode = EhourWebSession.getEhourConfig().isInDemoMode();
+
         Form<Void> form = new Form<>(id);
         form.setMultiPart(true);
 
@@ -79,32 +89,7 @@ public class RestoreDbFormPanel extends AbstractAjaxPanel<Void> {
 
         final ParseSession session = new ParseSession();
 
-        ProgressButton progressButton = new ProgressButton("submit", form, Model.of(taskContainer), new IRunnableFactory() {
-            @Override
-            public Runnable getRunnable() {
-                return new IProgressObservableRunnable() {
-                    @Override
-                    public double getProgress() {
-                        return session.getProgress();
-                    }
-
-                    @Override
-                    public String getProgressMessage() {
-                        return null;
-                    }
-
-                    @Override
-                    public void run() {
-                        try {
-                            restoreService.importDatabase(session);
-                        } catch (ImportException e) {
-                            LOGGER.error(e);
-                            throw new IllegalArgumentException(e);
-                        }
-                    }
-                };
-            }
-        }, Duration.milliseconds(500l)) {
+        ProgressButton progressButton = new ProgressButton("submit", form, Model.of(taskContainer), getRunnableFactory(session), Duration.milliseconds(500l)) {
             @Override
             protected void onTaskError(AjaxRequestTarget ajaxRequestTarget) {
                 ajaxRequestTarget.add(feedback);
@@ -123,16 +108,65 @@ public class RestoreDbFormPanel extends AbstractAjaxPanel<Void> {
                         LOGGER.error("While restoring", e);
                         error("Failed to write to temp file");
                     }
+
+                    this.setEnabled(false);
                 } else {
                     target.add(feedback);
                 }
             }
+
+            @Override
+            protected void updateAjaxAttributes(AjaxRequestAttributes attributes) {
+                super.updateAjaxAttributes(attributes);
+
+                List<IAjaxCallListener> callListeners = attributes.getAjaxCallListeners();
+
+                if (inDemoMode) {
+                    callListeners.add(new DemoDecorator());
+                } else {
+                    callListeners.add(new JavaScriptConfirmation(new ResourceModel("admin.import.restore.confirm")));
+                    callListeners.add(new LoadingSpinnerDecorator());
+                }
+            }
         };
+
+        progressButton.registerMessageModel(Model.of("Start"), InteractionState.STARTABLE);
+        progressButton.registerMessageModel(Model.of("Running..."), InteractionState.NON_INTERACTIVE);
+
         form.add(progressButton);
 
         form.add(new ProgressBar("bar", progressButton));
 
         return form;
+    }
+
+    private IRunnableFactory getRunnableFactory(final ParseSession session) {
+        return new IRunnableFactory() {
+            @Override
+            public Runnable getRunnable() {
+                return new IProgressObservableRunnable() {
+                    @Override
+                    public double getProgress() {
+                        return session.getProgress();
+                    }
+
+                    @Override
+                    public String getProgressMessage() {
+                        return "Restoring";
+                    }
+
+                    @Override
+                    public void run() {
+                        try {
+                            restoreService.importDatabase(session);
+                        } catch (ImportException e) {
+                            LOGGER.error(e);
+                            throw new IllegalArgumentException(e);
+                        }
+                    }
+                };
+            }
+        };
     }
 
     private boolean isValidUpload(FileUploadField field) {
